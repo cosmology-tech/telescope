cc76db2833c2570e2724deb6ec6a6fc7d00d0adb
Dan Lynch <pyramation@gmail.com>
2022-04-27 17:15:52 -0700

    add proto-parser

--- packages/transpiler/__tests__/transpile.test.ts
+++ packages/transpiler/__tests__/transpile.test.ts
@@ -1,3 +1,8 @@
-it('works', () => {
-    console.log('hi');
+import { ProtoResolver, ProtoStore } from '@osmonauts/proto-parser'
+const store = new ProtoStore(__dirname + '/../../../__fixtures__/chain1');
+const resolver = new ProtoResolver(store.getDeps());
+
+it('all', () => {
+    const all = resolver.resolveAll();
+    console.log(all);
 });
