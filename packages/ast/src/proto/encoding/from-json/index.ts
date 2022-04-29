0e59ff0ff02a5b0db35b6de97d9c2fd33fb548b2
Dan Lynch <pyramation@gmail.com>
2022-04-29 14:00:26 -0700

    from json

--- packages/ast/src/proto/encoding/from-json/index.ts
+++ packages/ast/src/proto/encoding/from-json/index.ts
@@ -3,6 +3,27 @@ import { camel, pascal } from 'case';
 import { identifier, tsEnumMember, tsPropertySignature, functionDeclaration, objectMethod } from '../../../utils';
 
 import { ProtoType } from '../../types';
+import { fromJsonTypes, mapArray, arrayTypes } from './utils';
+
+
+export const protoFromJSONMethodFields = (name: string, proto: ProtoType) => {
+    const fields = [
+        fromJsonTypes.Type('signDoc', 'SignDocDirectAux'),
+
+        fromJsonTypes.Enum('mode', 'signModeFromJSON'),
+
+        fromJsonTypes.string('sender'),
+
+        fromJsonTypes.Long('poolId'),
+
+        fromJsonTypes.bytes('queryData'),
+
+        mapArray('codeIds', arrayTypes.Long()),
+
+        mapArray('tokenInMaxs', arrayTypes.Type('Coin'))
+    ];
+    return fields;
+};
 
 export const protoFromJSONMethod = (name: string, proto: ProtoType) => {
     return objectMethod('method',
@@ -19,289 +40,7 @@ export const protoFromJSONMethod = (name: string, proto: ProtoType) => {
         t.blockStatement(
             [
                 t.returnStatement(
-                    t.objectExpression([
-
-                        /*
-
-                        signDoc: isSet(object.signDoc) ? SignDocDirectAux.fromJSON(object.signDoc) : undefined,
-
-                        */
-                        t.objectProperty(
-                            t.identifier('signDoc'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.identifier('isSet'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('signDoc')
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.identifier('SignDocDirectAux'),
-                                        t.identifier('fromJSON')
-                                    ),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('signDoc')
-                                        )
-                                    ]
-                                ),
-                                t.identifier('undefined')
-                            )
-                        ),
-
-                        // mode: isSet(object.mode) ? signModeFromJSON(object.mode) : 0,
-
-                        t.objectProperty(
-                            t.identifier('mode'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.identifier('isSet'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('mode')
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.identifier('signModeFromJSON'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('mode')
-                                        )
-                                    ]
-                                ),
-                                t.numericLiteral(0)
-                            )
-                        ),
-
-
-                        /*
-
-                        sender: isSet(object.sender) ? String(object.sender) : ""
-
-                        */
-                        t.objectProperty(
-                            t.identifier('sender'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.identifier('isSet'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('sender')
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.identifier('String'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('sender')
-                                        )
-                                    ]
-                                ),
-                                t.stringLiteral('')
-                            )
-                        ),
-
-                        /*
-
-                        poolId: isSet(object.poolId) ? Long.fromString(object.poolId) : Long.UZERO
-
-                        */
-
-                        t.objectProperty(
-                            t.identifier('poolId'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.identifier('isSet'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('poolId')
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.identifier('Long'),
-                                        t.identifier('fromString')
-                                    ),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('poolId')
-                                        )
-                                    ]
-                                ),
-                                t.memberExpression(
-                                    t.identifier('Long'),
-                                    t.identifier('UZERO')
-                                )
-                            )
-                        ),
-
-                        /*
-
-            queryData: isSet(object.queryData) ? bytesFromBase64(object.queryData) : new Uint8Array()
-                        
-                        */
-
-                        // TODO register import!
-                        // bytesFromBase64
-
-                        t.objectProperty(
-                            t.identifier('queryData'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.identifier('isSet'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('queryData')
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.identifier('bytesFromBase64'),
-                                    [
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('queryData')
-                                        )
-                                    ]
-                                ),
-                                t.newExpression(
-                                    t.identifier('Uint8Array'),
-                                    []
-                                )
-                            )
-                        ),
-
-                        /*
-
-                        Long[]
-
-codeIds: Array.isArray(object?.codeIds) ? object.codeIds.map((e: any) => Long.fromString(e)) : [],
-
-                        */
-
-
-                        t.objectProperty(
-                            t.identifier('codeIds'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.identifier('Array'),
-                                        t.identifier('isArray')
-                                    ),
-                                    [
-                                        t.optionalMemberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('codeIds'),
-                                            false,
-                                            true
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('codeIds')
-                                        ),
-                                        t.identifier('map')
-                                    ),
-                                    [
-                                        t.arrowFunctionExpression(
-                                            [
-                                                identifier('e', t.tsTypeAnnotation(
-                                                    t.tsAnyKeyword()
-                                                ))
-                                            ],
-                                            t.callExpression(
-                                                t.memberExpression(
-                                                    t.identifier('Long'),
-                                                    t.identifier('fromString')
-                                                ),
-                                                [
-                                                    t.identifier('e')
-                                                ]
-                                            ),
-                                            false
-                                        )
-                                    ]
-                                ),
-                                t.arrayExpression([])
-                            )
-                        ),
-
-
-
-                        /*
-        
-                        tokenInMaxs: Array.isArray(object?.tokenInMaxs) ? object.tokenInMaxs.map((e: any) => Coin.fromJSON(e)) : []
-
-                        */
-
-                        t.objectProperty(
-                            t.identifier('tokenInMaxs'),
-                            t.conditionalExpression(
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.identifier('Array'),
-                                        t.identifier('isArray')
-                                    ),
-                                    [
-                                        t.optionalMemberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('tokenInMaxs'),
-                                            false,
-                                            true
-                                        )
-                                    ]
-                                ),
-                                t.callExpression(
-                                    t.memberExpression(
-                                        t.memberExpression(
-                                            t.identifier('object'),
-                                            t.identifier('tokenInMaxs')
-                                        ),
-                                        t.identifier('map')
-                                    ),
-                                    [
-                                        t.arrowFunctionExpression(
-                                            [
-                                                identifier('e',
-                                                    t.tsTypeAnnotation(
-                                                        t.tsAnyKeyword()
-                                                    )
-                                                )
-                                            ],
-                                            t.callExpression(
-                                                t.memberExpression(
-                                                    t.identifier('Coin'),
-                                                    t.identifier('fromJSON')
-                                                ),
-                                                [
-                                                    t.identifier('e')
-                                                ]
-                                            )
-                                        )
-                                    ]
-                                ),
-                                t.arrayExpression([])
-                            )
-                        )
-                    ])
+                    t.objectExpression(protoFromJSONMethodFields(name, proto))
                 )
             ]
         ),
@@ -310,7 +49,7 @@ codeIds: Array.isArray(object?.codeIds) ? object.codeIds.map((e: any) => Long.fr
         false,
         t.tsTypeAnnotation(
             t.tsTypeReference(
-                t.identifier('MsgJoinPool')
+                t.identifier(name)
             )
         )
     )
--- packages/ast/src/proto/encoding/from-json/utils.ts
+++ packages/ast/src/proto/encoding/from-json/utils.ts
@@ -1,31 +1,232 @@
 import * as t from '@babel/types';
+import { identifier } from '../../../utils';
 
 export const fromJsonTypes = {
 
-    // message.sender = reader.string();
-    string() {
-
+    // sender: isSet(object.sender) ? String(object.sender) : ""
+    string(prop: string) {
+        return t.objectProperty(
+            t.identifier(prop),
+            t.conditionalExpression(
+                t.callExpression(
+                    t.identifier('isSet'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.callExpression(
+                    t.identifier('String'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.stringLiteral('')
+            )
+        )
     },
 
-    // message.poolId = (reader.uint64() as Long);
-    Long() {
-
+    // poolId: isSet(object.poolId) ? Long.fromString(object.poolId) : Long.UZERO
+    Long(prop: string) {
+        return t.objectProperty(
+            t.identifier(prop),
+            t.conditionalExpression(
+                t.callExpression(
+                    t.identifier('isSet'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.callExpression(
+                    t.memberExpression(
+                        t.identifier('Long'),
+                        t.identifier('fromString')
+                    ),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.memberExpression(
+                    t.identifier('Long'),
+                    t.identifier('UZERO')
+                )
+            )
+        );
     },
 
-    // message.signDoc = SignDocDirectAux.decode(reader, reader.uint32());
-    Type(name: string) {
-
+    // signDoc: isSet(object.signDoc) ? SignDocDirectAux.fromJSON(object.signDoc) : undefined,
+    Type(prop: string, name: string) {
+        return t.objectProperty(
+            t.identifier(prop),
+            t.conditionalExpression(
+                t.callExpression(
+                    t.identifier('isSet'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.callExpression(
+                    t.memberExpression(
+                        t.identifier(name),
+                        t.identifier('fromJSON')
+                    ),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.identifier('undefined')
+            )
+        );
     },
 
-    // message.mode = (reader.int32() as any);
-    Enum() {
-
+    // mode: isSet(object.mode) ? signModeFromJSON(object.mode) : 0,
+    Enum(prop: string, fromJSONFuncName: string) {
+        return t.objectProperty(
+            t.identifier(prop),
+            t.conditionalExpression(
+                t.callExpression(
+                    t.identifier('isSet'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.callExpression(
+                    t.identifier(fromJSONFuncName),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.numericLiteral(0)
+            )
+        );
     },
 
-    bytes() {
+    // TODO register import!
+    // bytesFromBase64
+    // queryData: isSet(object.queryData) ? bytesFromBase64(object.queryData) : new Uint8Array()
 
+    bytes(prop: string) {
+        return t.objectProperty(
+            t.identifier(prop),
+            t.conditionalExpression(
+                t.callExpression(
+                    t.identifier('isSet'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.callExpression(
+                    t.identifier('bytesFromBase64'),
+                    [
+                        t.memberExpression(
+                            t.identifier('object'),
+                            t.identifier(prop)
+                        )
+                    ]
+                ),
+                t.newExpression(
+                    t.identifier('Uint8Array'),
+                    []
+                )
+            )
+        );
     }
 
 };
 
+export const arrayTypes = {
+    // codeIds: Array.isArray(object?.codeIds) ? object.codeIds.map((e: any) => Long.fromString(e)) : [],
+    Long() {
+        return t.callExpression(
+            t.memberExpression(
+                t.identifier('Long'),
+                t.identifier('fromString')
+            ),
+            [
+                t.identifier('e')
+            ]
+        );
+    },
+    // tokenInMaxs: Array.isArray(object?.tokenInMaxs) ? object.tokenInMaxs.map((e: any) => Coin.fromJSON(e)) : []
+    Type(name) {
+        return t.callExpression(
+            t.memberExpression(
+                t.identifier(name),
+                t.identifier('fromJSON')
+            ),
+            [
+                t.identifier('e')
+            ]
+        );
+    }
+};
 
+// codeIds: Array.isArray(object?.codeIds) ? object.codeIds.map((e: any) => Long.fromString(e)) : [],
+export const mapArray = (prop: string, expr: t.Expression) => {
+    return t.objectProperty(
+        t.identifier(prop),
+        t.conditionalExpression(
+            t.callExpression(
+                t.memberExpression(
+                    t.identifier('Array'),
+                    t.identifier('isArray')
+                ),
+                [
+                    t.optionalMemberExpression(
+                        t.identifier('object'),
+                        t.identifier(prop),
+                        false,
+                        true
+                    )
+                ]
+            ),
+            t.callExpression(
+                t.memberExpression(
+                    t.memberExpression(
+                        t.identifier('object'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('map')
+                ),
+                [
+                    t.arrowFunctionExpression(
+                        [
+                            identifier('e', t.tsTypeAnnotation(
+                                t.tsAnyKeyword()
+                            ))
+                        ],
+                        expr,
+                        false
+                    )
+                ]
+            ),
+            t.arrayExpression([])
+        )
+    )
+};
