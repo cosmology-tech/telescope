aa921f421b258c92f787e93289ce1533deac44c4
Dan Lynch <pyramation@gmail.com>
2022-04-29 14:35:28 -0700

    to-json

--- packages/ast/src/proto/encoding/to-json/index.ts
+++ packages/ast/src/proto/encoding/to-json/index.ts
@@ -1,8 +1,20 @@
 import * as t from '@babel/types';
-import { camel, pascal } from 'case';
-import { identifier, tsEnumMember, tsPropertySignature, functionDeclaration, objectMethod } from '../../../utils';
-
+import { identifier, objectMethod } from '../../../utils';
 import { ProtoType } from '../../types';
+import { arrayToJson, arrayTypes, toJsonTypes } from './utils';
+
+export const protoToJSONMethodFields = (name: string, proto: ProtoType) => {
+    const fields = [
+        toJsonTypes.string('sender'),
+        toJsonTypes.Type('signDoc', 'SignDocDirectAux'),
+        toJsonTypes.Enum('mode', 'signModeToJSON'),
+        toJsonTypes.Long('poolId'),
+        arrayToJson('codeIds', arrayTypes.Long()),
+        toJsonTypes.bytes('queryData'),
+        arrayToJson('tokenInMaxs', arrayTypes.Type('Coin')),
+    ];
+    return fields;
+};
 
 export const protoToJSONMethod = (name: string, proto: ProtoType) => {
     return objectMethod('method',
@@ -12,7 +24,7 @@ export const protoToJSONMethod = (name: string, proto: ProtoType) => {
                 'message',
                 t.tsTypeAnnotation(
                     t.tsTypeReference(
-                        t.identifier('MsgJoinPool')
+                        t.identifier(name)
                     )
                 )
             )
@@ -28,349 +40,9 @@ export const protoToJSONMethod = (name: string, proto: ProtoType) => {
                 ]
             ),
 
-            /*
-                message.sender !== undefined && (obj.sender = message.sender);
-            */
-
-            t.expressionStatement(
-                t.logicalExpression(
-                    '&&',
-                    t.binaryExpression(
-                        '!==',
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('sender')
-                        ),
-                        t.identifier('undefined')
-                    ),
-                    t.assignmentExpression(
-                        '=',
-                        t.memberExpression(
-                            t.identifier('obj'),
-                            t.identifier('sender')
-                        ),
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('sender')
-                        )
-                    )
-                )
-            ),
-
-            /*
-                
-                message.signDoc !== undefined && (obj.signDoc = message.signDoc ? SignDocDirectAux.toJSON(message.signDoc) : undefined);
-            */
-
-            t.expressionStatement(
-                t.logicalExpression(
-                    '&&',
-                    t.binaryExpression(
-                        '!==',
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('signDoc')
-                        ),
-                        t.identifier('undefined')
-                    ),
-                    t.assignmentExpression(
-                        '=',
-                        t.memberExpression(
-                            t.identifier('obj'),
-                            t.identifier('signDoc')
-                        ),
-                        t.conditionalExpression(
-                            t.memberExpression(
-                                t.identifier('message'),
-                                t.identifier('signDoc')
-                            ),
-                            t.callExpression(
-                                t.memberExpression(
-                                    t.identifier('SignDocDirectAux'),
-                                    t.identifier('toJSON')
-                                ),
-                                [
-                                    t.memberExpression(
-                                        t.identifier('message'),
-                                        t.identifier('signDoc')
-                                    )
-                                ]
-                            ),
-                            t.identifier('undefined')
-                        )
-                    )
-                )
-            ),
-
-            // message.mode !== undefined && (obj.mode = signModeToJSON(message.mode));
-
-            t.expressionStatement(
-                t.logicalExpression(
-                    '&&',
-                    t.binaryExpression(
-                        '!==',
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('mode')
-                        ),
-                        t.identifier('undefined')
-                    ),
-                    t.assignmentExpression(
-                        '=',
-                        t.memberExpression(
-                            t.identifier('obj'),
-                            t.identifier('mode')
-                        ),
-                        t.callExpression(
-                            t.identifier('signModeToJSON'),
-                            [
-                                t.memberExpression(
-                                    t.identifier('message'),
-                                    t.identifier('mode')
-                                )
-                            ]
-                        )
-
-                    )
-                )
-            ),
-
-
-            /*
-     
-            message.poolId !== undefined && (obj.poolId = (message.poolId || Long.UZERO).toString());
-    
-            */
-
-            t.expressionStatement(
-                t.logicalExpression(
-                    '&&',
-                    t.binaryExpression(
-                        '!==',
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('poolId')
-                        ),
-                        t.identifier('undefined')
-                    ),
-                    t.assignmentExpression(
-                        '=',
-                        t.memberExpression(
-                            t.identifier('obj'),
-                            t.identifier('poolId'),
-                            false,
-                            false
-                        ),
-                        t.callExpression(
-                            t.memberExpression(
-                                t.logicalExpression(
-                                    '||',
-                                    t.memberExpression(
-                                        t.identifier('message'),
-                                        t.identifier('poolId')
-                                    ),
-                                    t.memberExpression(
-                                        t.identifier('Long'),
-                                        t.identifier('UZERO')
-                                    )
-                                ),
-                                t.identifier('toString')
-                            ),
-                            []
-                        )
-                    )
-                )
-            ),
-
-            /*
-                Long[]
-            */
-
-            t.ifStatement(
-                t.memberExpression(
-                    t.identifier('message'),
-                    t.identifier('codeIds')
-                ),
-                t.blockStatement([
-                    t.expressionStatement(
-                        t.assignmentExpression(
-                            '=',
-                            t.memberExpression(
-                                t.identifier('obj'),
-                                t.identifier('codeIds')
-                            ),
-                            t.callExpression(
-                                t.memberExpression(
-                                    t.memberExpression(
-                                        t.identifier('message'),
-                                        t.identifier('codeIds')
-                                    ),
-                                    t.identifier('map')
-                                ),
-                                [
-                                    t.arrowFunctionExpression(
-                                        [
-                                            t.identifier('e')
-                                        ],
-                                        t.callExpression(
-                                            t.memberExpression(
-                                                t.logicalExpression(
-                                                    '||',
-                                                    t.identifier('e'),
-                                                    t.memberExpression(
-                                                        t.identifier('Long'),
-                                                        t.identifier('UZERO')
-                                                    )
-                                                ),
-                                                t.identifier('toString')
-                                            ),
-                                            []
-                                        )
-                                    )
-                                ]
-                            )
-                        )
-                    )
-                ]),
-                t.blockStatement([
-                    t.expressionStatement(
-                        t.assignmentExpression(
-                            '=',
-                            t.memberExpression(
-                                t.identifier('obj'),
-                                t.identifier('codeIds')
-                            ),
-                            t.arrayExpression([])
-                        )
-                    )
-                ])
-            ),
-
-
-
-            /*
-     
-             message.queryData !== undefined && (obj.queryData = base64FromBytes(message.queryData !== undefined ? message.queryData : new Uint8Array()));
-    
-            */
-
-            t.expressionStatement(
-                t.logicalExpression(
-                    '&&',
-                    t.binaryExpression(
-                        '!==',
-                        t.memberExpression(
-                            t.identifier('message'),
-                            t.identifier('queryData')
-                        ),
-                        t.identifier('undefined')
-                    ),
-                    t.assignmentExpression(
-                        '=',
-                        t.memberExpression(
-                            t.identifier('obj'),
-                            t.identifier('queryData'),
-                            false,
-                            false
-                        ),
-                        t.callExpression(
-                            t.identifier('base64FromBytes'),
-                            [
-                                t.conditionalExpression(
-                                    t.binaryExpression(
-                                        '!==',
-                                        t.memberExpression(
-                                            t.identifier('message'),
-                                            t.identifier('queryData')
-                                        ),
-                                        t.identifier('undefined')
-                                    ),
-                                    t.memberExpression(
-                                        t.identifier('message'),
-                                        t.identifier('queryData')
-                                    ),
-                                    t.newExpression(t.identifier('Uint8Array'), []))
-                            ]
-                        )
-                    )
-                )
-            ),
-
-
-
-            /*
-    
-            if (message.tokenInMaxs) {
-                obj.tokenInMaxs = message.tokenInMaxs.map(e => e ? Coin.toJSON(e) : undefined);
-            } else {
-                obj.tokenInMaxs = [];
-            }
-    
-            */
-
-            t.ifStatement(
-                t.memberExpression(
-                    t.identifier('message'),
-                    t.identifier('tokenInMaxs')
-                ),
-                t.blockStatement([
-                    t.expressionStatement(
-                        t.assignmentExpression(
-                            '=',
-                            t.memberExpression(
-                                t.identifier('obj'),
-                                t.identifier('tokenInMaxs')
-                            ),
-                            t.callExpression(
-                                t.memberExpression(
-                                    t.memberExpression(
-                                        t.identifier('message'),
-                                        t.identifier('tokenInMaxs')
-                                    ),
-                                    t.identifier('map')
-                                ),
-                                [
-                                    t.arrowFunctionExpression(
-                                        [
-                                            t.identifier('e')
-                                        ],
-                                        t.conditionalExpression(
-                                            t.identifier('e'),
-                                            t.callExpression(
-                                                t.memberExpression(
-                                                    t.identifier('Coin'),
-                                                    t.identifier('toJSON')
-                                                ),
-                                                [
-                                                    t.identifier('e')
-                                                ]
-                                            ),
-                                            t.identifier('undefined')
-                                        )
-                                    )
-                                ]
-                            )
-                        )
-                    )
-                ]),
-                t.blockStatement([
-                    t.expressionStatement(
-                        t.assignmentExpression(
-                            '=',
-                            t.memberExpression(
-                                t.identifier('obj'),
-                                t.identifier('tokenInMaxs'),
-                                false,
-                                false
-                            ),
-                            t.arrayExpression([])
-                        )
-                    )
-                ])
-            ),
+            ...protoToJSONMethodFields(name, proto),
 
             // RETURN 
-
             t.returnStatement(t.identifier('obj'))
 
         ]),
--- packages/ast/src/proto/encoding/to-json/utils.ts
+++ packages/ast/src/proto/encoding/to-json/utils.ts
@@ -2,30 +2,299 @@ import * as t from '@babel/types';
 
 export const toJsonTypes = {
 
-    // message.sender = reader.string();
-    string() {
-
+    //  message.sender !== undefined && (obj.sender = message.sender);
+    string(prop: string) {
+        return t.expressionStatement(
+            t.logicalExpression(
+                '&&',
+                t.binaryExpression(
+                    '!==',
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('undefined')
+                ),
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop)
+                    ),
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    )
+                )
+            )
+        );
     },
 
-    // message.poolId = (reader.uint64() as Long);
-    Long() {
-
+    // message.poolId !== undefined && (obj.poolId = (message.poolId || Long.UZERO).toString());
+    Long(prop: string) {
+        return t.expressionStatement(
+            t.logicalExpression(
+                '&&',
+                t.binaryExpression(
+                    '!==',
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('undefined')
+                ),
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop),
+                        false,
+                        false
+                    ),
+                    t.callExpression(
+                        t.memberExpression(
+                            t.logicalExpression(
+                                '||',
+                                t.memberExpression(
+                                    t.identifier('message'),
+                                    t.identifier(prop)
+                                ),
+                                t.memberExpression(
+                                    t.identifier('Long'),
+                                    t.identifier('UZERO')
+                                )
+                            ),
+                            t.identifier('toString')
+                        ),
+                        []
+                    )
+                )
+            )
+        );
     },
 
-    // message.signDoc = SignDocDirectAux.decode(reader, reader.uint32());
-    Type(name: string) {
-
+    // message.signDoc !== undefined && (obj.signDoc = message.signDoc ? SignDocDirectAux.toJSON(message.signDoc) : undefined);
+    Type(prop: string, name: string) {
+        return t.expressionStatement(
+            t.logicalExpression(
+                '&&',
+                t.binaryExpression(
+                    '!==',
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('undefined')
+                ),
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop)
+                    ),
+                    t.conditionalExpression(
+                        t.memberExpression(
+                            t.identifier('message'),
+                            t.identifier(prop)
+                        ),
+                        t.callExpression(
+                            t.memberExpression(
+                                t.identifier(name),
+                                t.identifier('toJSON')
+                            ),
+                            [
+                                t.memberExpression(
+                                    t.identifier('message'),
+                                    t.identifier(prop)
+                                )
+                            ]
+                        ),
+                        t.identifier('undefined')
+                    )
+                )
+            )
+        );
     },
 
-    // message.mode = (reader.int32() as any);
-    Enum() {
+    // message.mode !== undefined && (obj.mode = signModeToJSON(message.mode));
+    Enum(prop: string, enumFuncName: string) {
+        return t.expressionStatement(
+            t.logicalExpression(
+                '&&',
+                t.binaryExpression(
+                    '!==',
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('undefined')
+                ),
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop)
+                    ),
+                    t.callExpression(
+                        t.identifier(enumFuncName),
+                        [
+                            t.memberExpression(
+                                t.identifier('message'),
+                                t.identifier(prop)
+                            )
+                        ]
+                    )
 
+                )
+            )
+        );
     },
 
-    bytes() {
-
+    // message.queryData !== undefined && (obj.queryData = base64FromBytes(message.queryData !== undefined ? message.queryData : new Uint8Array()));
+    bytes(prop: string) {
+        return t.expressionStatement(
+            t.logicalExpression(
+                '&&',
+                t.binaryExpression(
+                    '!==',
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(prop)
+                    ),
+                    t.identifier('undefined')
+                ),
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop),
+                        false,
+                        false
+                    ),
+                    t.callExpression(
+                        t.identifier('base64FromBytes'),
+                        [
+                            t.conditionalExpression(
+                                t.binaryExpression(
+                                    '!==',
+                                    t.memberExpression(
+                                        t.identifier('message'),
+                                        t.identifier(prop)
+                                    ),
+                                    t.identifier('undefined')
+                                ),
+                                t.memberExpression(
+                                    t.identifier('message'),
+                                    t.identifier(prop)
+                                ),
+                                t.newExpression(t.identifier('Uint8Array'), []))
+                        ]
+                    )
+                )
+            )
+        );
     }
 
 };
 
+export const arrayTypes = {
+    // if (message.codeIds) {
+    //     obj.codeIds = message.codeIds.map(e => (e || Long.UZERO).toString());
+    // } else {
+    //     obj.codeIds = [];
+    // }
 
+    Long() {
+        return t.callExpression(
+            t.memberExpression(
+                t.logicalExpression(
+                    '||',
+                    t.identifier('e'),
+                    t.memberExpression(
+                        t.identifier('Long'),
+                        t.identifier('UZERO')
+                    )
+                ),
+                t.identifier('toString')
+            ),
+            []
+        )
+    },
+
+    // if (message.tokenInMaxs) {
+    //     obj.tokenInMaxs = message.tokenInMaxs.map(e => e ? Coin.toJSON(e) : undefined);
+    // } else {
+    //     obj.tokenInMaxs = [];
+    // }
+
+    Type(name: string) {
+        return t.conditionalExpression(
+            t.identifier('e'),
+            t.callExpression(
+                t.memberExpression(
+                    t.identifier('Coin'),
+                    t.identifier('toJSON')
+                ),
+                [
+                    t.identifier('e')
+                ]
+            ),
+            t.identifier('undefined')
+        );
+    }
+}
+
+// if (message.codeIds) {
+//     obj.codeIds = message.codeIds.map(e => (e || Long.UZERO).toString());
+// } else {
+//     obj.codeIds = [];
+// }
+
+export const arrayToJson = (prop: string, expr: t.Expression) => {
+    return t.ifStatement(
+        t.memberExpression(
+            t.identifier('message'),
+            t.identifier(prop)
+        ),
+        t.blockStatement([
+            t.expressionStatement(
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop)
+                    ),
+                    t.callExpression(
+                        t.memberExpression(
+                            t.memberExpression(
+                                t.identifier('message'),
+                                t.identifier(prop)
+                            ),
+                            t.identifier('map')
+                        ),
+                        [
+                            t.arrowFunctionExpression(
+                                [
+                                    t.identifier('e')
+                                ],
+                                expr
+                            )
+                        ]
+                    )
+                )
+            )
+        ]),
+        t.blockStatement([
+            t.expressionStatement(
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(prop)
+                    ),
+                    t.arrayExpression([])
+                )
+            )
+        ])
+    );
+};
