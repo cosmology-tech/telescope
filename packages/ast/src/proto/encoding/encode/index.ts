a3315ca08c099cee82a57a234a6644aac4ebaf3b
Dan Lynch <pyramation@gmail.com>
2022-04-29 17:13:09 -0700

    encode

--- packages/ast/src/proto/encoding/encode/index.ts
+++ packages/ast/src/proto/encoding/encode/index.ts
@@ -1,22 +1,85 @@
 import * as t from '@babel/types';
 import { identifier, objectMethod } from '../../../utils';
-import { ProtoType } from '../../types';
+import { ProtoType, ProtoField } from '../../types';
 import { encode, arrayTypes } from './utils';
 
+const needsImplementation = (name: string, field: ProtoField) => {
+    throw new Error(`need to implement encode (${field.type} rules[${field.rule}] name[${name}])`);
+}
+
 export const protoEncodeMethodFields = (name: string, proto: ProtoType) => {
-    const fields = [
-        encode.string(10, 'sender'),
-        encode.long(16, 'poolId'),
-        encode.bytes(18, 'queryData'),
-        ...encode.scalarArray(10, 'codeIds', arrayTypes.long()),
-        encode.type(18, 'signDoc', 'SignDocDirectAux'),
-        encode.enum(24, 'mode'),
-        ...encode.typeArray(34, 'tokenInMaxs', 'Coin')
-    ];
-    return fields;
+
+    // TODO figure this out
+    const calcTagId = (fieldId) => {
+        return fieldId + 10;
+    }
+
+    return Object.keys(proto.fields ?? {}).reduce((m, fieldName) => {
+        const field = proto.fields[fieldName];
+        if (field.rule === 'repeated') {
+            switch (field.type) {
+                case 'string':
+                    return needsImplementation(fieldName, field);
+                case 'uint64':
+                    return [...m, ...encode.scalarArray(calcTagId(field.id), fieldName, arrayTypes.long())];
+                case 'int64':
+                    return needsImplementation(fieldName, field);
+                case 'bytes':
+                    return needsImplementation(fieldName, field);
+                default:
+                    switch (field.parsedType.type) {
+                        case 'Enum':
+                            // could be same as Type?
+                            return needsImplementation(fieldName, field);
+                        case 'Type':
+                            return [...m, ...encode.typeArray(calcTagId(field.id), fieldName, field.parsedType.name)];
+                    }
+                    return needsImplementation(fieldName, field);
+            }
+
+        }
+
+        switch (field.type) {
+            case 'string':
+                return [...m, encode.string(calcTagId(field.id), fieldName)];
+            case 'uint64':
+                return [...m, encode.long(calcTagId(field.id), fieldName)];
+            case 'int64':
+                return needsImplementation(fieldName, field);
+            case 'bytes':
+                return [...m, encode.bytes(calcTagId(field.id), fieldName)];
+            default:
+                switch (field.parsedType.type) {
+                    case 'Enum':
+                        return [...m, encode.enum(calcTagId(field.id), fieldName)];
+                    case 'Type':
+                        return [...m, encode.type(calcTagId(field.id), fieldName, field.parsedType.name)];
+                }
+                return needsImplementation(fieldName, field);
+        }
+    }, []);
 };
 
 export const protoEncodeMethod = (name: string, proto: ProtoType) => {
+    const fields = protoEncodeMethodFields(name, proto);
+    if (!fields.length) {
+        console.log('no fields?');
+    }
+    const body = [
+        ...fields,
+        /* RETURN writer */
+        t.returnStatement(
+            t.identifier('writer')
+        )
+    ];
+
+    try {
+        t.blockStatement(body);
+    } catch (e) {
+        console.log(body);
+        throw e;
+    }
+
     return objectMethod(
         'method',
         t.identifier('encode'),
@@ -47,17 +110,7 @@ export const protoEncodeMethod = (name: string, proto: ProtoType) => {
         ],
 
         // body 
-        t.blockStatement([
-
-            ...protoEncodeMethodFields(name, proto),
-
-            /* RETURN writer */
-
-            t.returnStatement(
-                t.identifier('writer')
-            )
-
-        ]),
+        t.blockStatement(body),
         false,
         false,
         false,
