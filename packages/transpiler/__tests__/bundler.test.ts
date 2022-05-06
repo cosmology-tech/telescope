955eb554e393a05d56dd3a617985c335992774e9
Dan Lynch <pyramation@gmail.com>
2022-05-06 13:23:04 -0700

    bundle test

--- packages/transpiler/__tests__/bundler.test.ts
+++ packages/transpiler/__tests__/bundler.test.ts
@@ -1,16 +1,36 @@
 import { TelescopeBuilder } from '../src';
+import * as t from '@babel/types';
+
 import { ProtoStore } from '@osmonauts/proto-parser'
-import { getPackagesBundled } from '../src/bundle'
+import { bundlePackages, getPackagesBundled } from '../src/bundle'
+import generate from '@babel/generator';
 
 const store = new ProtoStore(__dirname + '/../../../__fixtures__/chain1');
 store.traverseAll();
 
-const telescope = new TelescopeBuilder({
+const input = {
     outPath: __dirname + '/../../../__fixtures__/output1',
     protoDir: __dirname + '/../../../__fixtures__/chain1'
+};
+// const telescope = new TelescopeBuilder(input);
+
+it('getPackagesBundled', () => {
+    const bundle = getPackagesBundled(store);
+    expect(Object.keys(bundle)).toEqual(["ics23", "cosmos_proto", "cosmos", "cosmwasm", "gogoproto", "google", "ibc", "osmosis", "secret", "tendermint"]);
 });
 
-it('bundle', () => {
-    const bundle = getPackagesBundled(store);
-    console.log(bundle);
-});
+it('bundlePackages', () => {
+    const bundled = bundlePackages(store, input);
+    const packaged = bundled.reduce((m, bundle) => {
+        m[bundle.base] = {
+            bundle: '.' + bundle.bundleFile.split('__fixtures__')[1],
+            base: bundle.base,
+            code: generate(t.program([
+                ...bundle.importPaths,
+                ...bundle.body
+            ])).code
+        };
+        return m;
+    }, {});
+    expect(packaged).toMatchSnapshot();
+})
--- packages/transpiler/src/bundle.ts
+++ packages/transpiler/src/bundle.ts
@@ -1,5 +1,8 @@
-import { ProtoStore } from "@osmonauts/proto-parser"
+import { ProtoStore } from '@osmonauts/proto-parser';
+import { recursiveModuleBundle, importNamespace } from '@osmonauts/ast';
 import * as dotty from 'dotty';
+import { TelescopeInput } from './index';
+import { join, relative, dirname } from 'path';
 
 // TODO move to store
 export const getPackages = (store: ProtoStore) => {
@@ -24,4 +27,50 @@ export const getPackagesBundled = (store: ProtoStore) => {
     });
     return objectified;
 
+}
+
+export const bundlePackages = (store: ProtoStore, input: TelescopeInput) => {
+    const allPackages = getPackagesBundled(store);
+    return Object.keys(allPackages).map(base => {
+        const pkgs = allPackages[base];
+        const bundleVariables = {};
+        const bundleFile = join(input.protoDir, base, 'bundle.ts');
+        const importPaths = [];
+        parsePackage(pkgs, bundleFile, importPaths, bundleVariables);
+        const body = recursiveModuleBundle(bundleVariables);
+        return {
+            bundleFile,
+            importPaths,
+            base,
+            body
+        };
+    });
+}
+
+export const parsePackage = (obj, bundleFile, importPaths, bundleVariables) => {
+    if (!obj) return;
+    if (obj.pkg && obj.files) {
+        obj.files.forEach(file => {
+            createFileBundle(obj, file.filename, bundleFile, importPaths, bundleVariables);
+            // TODO better namespacing for aminos/messages/encoding:
+            // file.outFiles.forEach(outFile =>
+            //     createFileBundle(obj, outFile.filename, bundleFile, importPaths, bundleVariables)
+            // );
+        });
+        return;
+    }
+    Object.keys(obj).forEach(mini => {
+        parsePackage(obj[mini], bundleFile, importPaths, bundleVariables);
+    })
+}
+
+
+let counter = 0;
+const createFileBundle = (obj, filename, bundleFile, importPaths, bundleVariables) => {
+    let rel = relative(dirname(bundleFile), filename);
+    if (!rel.startsWith('.')) rel = `./${rel}`;
+    const variable = `_${counter++}`;
+    importPaths.push(importNamespace(variable, rel));
+    dotty.put(bundleVariables, obj.pkg + '.__export', true);
+    dotty.put(bundleVariables, obj.pkg + '.' + variable, true);
 }
