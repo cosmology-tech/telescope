8e2afc1b4d444f7474109f7dd133383b9b3e504a
Dan Lynch <pyramation@gmail.com>
2022-06-14 21:28:06 -0700

    lint

--- packages/osmojs/scripts/gen-proto.js
+++ packages/osmojs/scripts/gen-proto.js
@@ -2,27 +2,25 @@ import { join } from 'path';
 import telescope from '@osmonauts/telescope';
 import { camel, snake } from 'case';
 
-const protoDirs = [
-    join(__dirname, '/../proto')
-];
+const protoDirs = [join(__dirname, '/../proto')];
 const outPath = join(__dirname, '/../src/proto');
 
 telescope({
-    protoDirs,
-    outPath,
-    options: {
-        includeAminos: true,
-        includeLCDClient: false
-    },
-    plugins: [
-        {
-            name: 'aminoCasing',
-            plugin: ({protoPackage}) => {
-                if (protoPackage.startsWith('osmosis')) {
-                    return camel;
-                }
-                return snake;
-            }
+  protoDirs,
+  outPath,
+  options: {
+    includeAminos: true,
+    includeLCDClient: false
+  },
+  plugins: [
+    {
+      name: 'aminoCasing',
+      plugin: ({ protoPackage }) => {
+        if (protoPackage.startsWith('osmosis')) {
+          return camel;
         }
-    ]
+        return snake;
+      }
+    }
+  ]
 });
