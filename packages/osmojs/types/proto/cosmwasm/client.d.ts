d38aa80a8bfd4af21d9a625b2be93e1037f2ef62
Dan Lynch <pyramation@gmail.com>
2022-07-12 13:28:52 -0700

    remove

--- packages/osmojs/types/proto/cosmwasm/client.d.ts
+++
@@ -1,7 +0,0 @@
-import { OfflineSigner, GeneratedType } from "@cosmjs/proto-signing";
-import { SigningStargateClient } from "@cosmjs/stargate";
-export declare const getSigningCosmwasmClient: ({ rpcEndpoint, signer, defaultTypes }: {
-    rpcEndpoint: string;
-    signer: OfflineSigner;
-    defaultTypes?: ReadonlyArray<[string, GeneratedType]>;
-}) => Promise<SigningStargateClient>;
