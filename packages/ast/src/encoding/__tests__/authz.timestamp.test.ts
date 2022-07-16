39a6cbda031bdd6f77f165c8e4b76d5abcc54126
Dan Lynch <pyramation@gmail.com>
2022-07-15 17:15:43 -0700

    proto + amino tests

--- packages/ast/src/encoding/__tests__/authz.timestamp.test.ts
+++ packages/ast/src/encoding/__tests__/authz.timestamp.test.ts
@@ -2,9 +2,10 @@ import { getNestedProto } from '@osmonauts/proto-parser';
 import { defaultTelescopeOptions } from '@osmonauts/types';
 import { ProtoStore } from '@osmonauts/proto-parser'
 import { expectCode } from '../../../test-utils/'
-import { ProtoParseContext } from '../context';
+import { AminoParseContext, ProtoParseContext } from '../context';
 import { createProtoType } from '..';
 import { createObjectWithMethods } from '../object';
+import { createAminoConverter } from '../amino/index';
 
 const store = new ProtoStore([__dirname + '/../../../../../__fixtures__/chain1']);
 store.traverseAll();
@@ -12,16 +13,25 @@ store.traverseAll();
 describe('Authz', () => {
     const ref = store.findProto('cosmos/authz/v1beta1/authz.proto');
     const context = new ProtoParseContext(ref, store, defaultTelescopeOptions);
+    const aminoCtx = new AminoParseContext(ref, store, defaultTelescopeOptions);
     describe('timestamp', () => {
         it('interface', () => {
             expectCode(createProtoType(context, 'Grant',
                 getNestedProto(ref.traversed).Grant
             ));
         });
-        it('encoder', () => {
+        it('proto', () => {
             expectCode(createObjectWithMethods(context, 'Grant',
                 getNestedProto(ref.traversed).Grant
             ));
+        });
+        it('amino', () => {
+            expectCode(createAminoConverter({
+                context: aminoCtx,
+                root: ref.proto,
+                name: 'AminoConverter',
+                protos: [getNestedProto(ref.traversed).Grant]
+            }));
         });
     })
 });
