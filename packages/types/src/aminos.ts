016fc9c403ef49fbc31006f494e1e397261be4c6
Dan Lynch <pyramation@gmail.com>
2022-07-18 10:27:26 -0700

    NFT name conflict

--- packages/types/src/aminos.ts
+++ packages/types/src/aminos.ts
@@ -13,5 +13,9 @@ export const DEFAULT_AMINO_EXCEPTIONS: AminoExceptions = {
     },
     '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward': {
         aminoType: 'cosmos-sdk/MsgWithdrawDelegationReward'
+    },
+    '/cosmos.nft.v1beta1.MsgSend': {
+        // TODO find aminoType name
+        aminoType: 'cosmos-sdk/MsgNFTSend'
     }
 }
