c3a7279e34ccba1958a079cff8fc0b76eb7dd31f
Dan Lynch <pyramation@gmail.com>
2022-07-27 01:22:47 -1000

    readme

--- packages/ast/types/src/encoding/amino/utils.d.ts
+++ packages/ast/types/src/encoding/amino/utils.d.ts
@@ -2,7 +2,7 @@ import { ProtoAny, ProtoRoot, ProtoType } from '@osmonauts/types';
 import { GenericParseContext } from '../context';
 export declare const getTypeUrl: (root: ProtoRoot, proto: ProtoAny | ProtoType) => string;
 export declare const arrayTypeNDim: (body: any, n: any) => any;
-export declare const typeUrlToAmino: (context: GenericParseContext, typeUrl: string) => string;
+export declare const typeUrlToAmino: (context: GenericParseContext, typeUrl: string) => any;
 export declare const protoFieldsToArray: (proto: ProtoType) => {
     type?: string;
     name: string;
