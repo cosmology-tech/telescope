993e38f9c3a4563b280967578ff400c5316cc005
Dan Lynch <pyramation@gmail.com>
2022-11-22 23:02:55 -0800

    hooks

--- packages/ast/src/state/react-query/react-query.test.ts
+++ packages/ast/src/state/react-query/react-query.test.ts
@@ -2,7 +2,9 @@ import { traverse, getNestedProto } from '@osmonauts/proto-parser'
 import { defaultTelescopeOptions, ProtoService } from '@osmonauts/types';
 import { expectCode, getTestProtoStore } from '../../../test-utils';
 import { GenericParseContext } from '../../encoding';
-import { createRpcQueryHooks } from './react-query';
+import { createRpcQueryHooks, createRpcQueryHookInterfaces } from './react-query';
+import * as t from '@babel/types';
+
 const store = getTestProtoStore();
 store.traverseAll();
 
@@ -12,4 +14,14 @@ it('createRpcQueryHooks', () => {
     const service: ProtoService = getNestedProto(res).Query;
     const context = new GenericParseContext(ref, store, defaultTelescopeOptions);
     expectCode(createRpcQueryHooks(context, service))
+});
+
+it('createRpcQueryHookInterfaces', () => {
+    const ref = store.findProto('cosmos/auth/v1beta1/query.proto');
+    const res = traverse(store, ref);
+    const service: ProtoService = getNestedProto(res).Query;
+    const context = new GenericParseContext(ref, store, defaultTelescopeOptions);
+    expectCode(t.program([
+        ...createRpcQueryHookInterfaces(context, service)
+    ]))
 });
--- packages/ast/src/state/react-query/react-query.ts
+++ packages/ast/src/state/react-query/react-query.ts
@@ -1,10 +1,9 @@
 
 import * as t from '@babel/types';
-import { callExpression, identifier, makeCommentBlock, makeCommentLine, makeCommentLineWithBlocks, objectMethod, objectPattern, objectProperty } from '../../utils';
+import { callExpression, identifier, makeCommentBlock, makeCommentLineWithBlocks, objectMethod, objectPattern, objectProperty } from '../../utils';
 import { ProtoService, ProtoServiceMethod } from '@osmonauts/types';
 import { GenericParseContext } from '../../encoding';
 import { camel } from '@osmonauts/utils';
-import { getRpcClassName } from '../../clients/rpc/class';
 import { pascal } from 'case';
 
 const makeUseHookName = (name: string) => {
@@ -38,17 +37,6 @@ const rpcHookMethod = (
     // also default to empty
     optional = true;
   }
-
-  // const methodArgs: t.Identifier = identifier(
-  //     'request',
-  //     t.tsTypeAnnotation(
-  //         t.tsTypeReference(
-  //             t.identifier(requestType)
-  //         )
-  //     ),
-  //     optional
-  // );
-
 
   context.addUtil('useQuery');
 
@@ -115,6 +103,49 @@ const rpcHookMethod = (
         ]))
     )
   ]);
+
+}
+
+const rpcHookMethodInterface = (
+  context: GenericParseContext,
+  name: string,
+  svc: ProtoServiceMethod
+) => {
+  const requestType = svc.requestType;
+  const responseType = svc.responseType;
+  const fieldNames = Object.keys(svc.fields ?? {})
+  const hasParams = fieldNames.length > 0;
+
+  let optional = false;
+  // // if no params, then let's default to empty object for cleaner API
+  if (!hasParams) {
+    optional = true;
+  } else if (hasParams && fieldNames.length === 1 && fieldNames.includes('pagination')) {
+    // if only argument "required" is pagination
+    // also default to empty
+    optional = true;
+  }
+
+  context.addUtil('ReactQueryParams');
+
+  return t.exportNamedDeclaration(t.tsInterfaceDeclaration(
+    t.identifier('UseBalanceQuery'),
+    t.tsTypeParameterDeclaration([
+      t.tsTypeParameter(null, null, 'TData')
+    ]),
+    [
+      t.tsExpressionWithTypeArguments(
+        t.identifier('ReactQueryParams'),
+        t.tsTypeParameterInstantiation([
+          t.tsTypeReference(t.identifier(responseType)),
+          t.tsTypeReference(t.identifier('TData'))
+        ])
+      )
+    ],
+    t.tsInterfaceBody([
+
+    ])
+  ))
 
 }
 
@@ -207,5 +238,27 @@ export const createRpcQueryHooks = (
       )
     ])
   );
+};
+
+
+export const createRpcQueryHookInterfaces = (
+  context: GenericParseContext,
+  service: ProtoService
+) => {
+
+
+  const camelRpcMethods = context.pluginValue('rpcClients.camelCase');
+
+  const methods = Object.keys(service.methods ?? {})
+    .map(key => {
+      const name = camelRpcMethods ? camel(key) : key;
+      const method = service.methods[key];
+      return {
+        name,
+        method
+      };
+    });
+
+  return methods.map(method => rpcHookMethodInterface(context, method.name, method.method));
 };
 
