6af71005869f2d30f2ede526c53f626cc84d0f9b
Dan Lynch <pyramation@gmail.com>
2022-11-24 11:57:12 -0800

    updates for cosmjs-types

--- packages/telescope/src/helpers/internal.ts
+++ packages/telescope/src/helpers/internal.ts
@@ -156,6 +156,8 @@ type Builtin = Date | Function | Uint8Array | string | number | boolean | undefi
 
 export type DeepPartial<T> = T extends Builtin
     ? T
+    : T extends Long
+    ? string | number | Long
     : T extends Array<infer U>
     ? Array<DeepPartial<U>>
     : T extends ReadonlyArray<infer U>
