dc9486c85d77b16dfd464ba70d8437297589bbcc
Dan Lynch <pyramation@gmail.com>
2022-11-25 20:36:43 -0800

    add from/to amino methods

---
+++ packages/ast/src/encoding/proto/to-amino/index.ts
@@ -0,0 +1,199 @@
+import * as t from '@babel/types';
+import { getFieldOptionality, getOneOfs } from '..';
+import { identifier, objectMethod } from '../../../utils';
+import { ProtoParseContext } from '../../context';
+import { ProtoField, ProtoType } from '@osmonauts/types';
+import { arrayTypes, toAminoJSON } from './utils';
+import { pascal } from 'case';
+
+const needsImplementation = (name: string, field: ProtoField) => {
+    throw new Error(`need to implement toAminoJSON (${field.type} rules[${field.rule}] name[${name}])`);
+}
+
+export interface ToAminoJSONMethod {
+    context: ProtoParseContext;
+    field: ProtoField;
+    isOptional: boolean;
+}
+
+export const toAminoJSONMethodFields = (context: ProtoParseContext, name: string, proto: ProtoType) => {
+    const oneOfs = getOneOfs(proto);
+    const fields = Object.keys(proto.fields ?? {}).reduce((m, fieldName) => {
+        const field = {
+            name: fieldName,
+            ...proto.fields[fieldName]
+        };
+
+        const isOneOf = oneOfs.includes(fieldName);
+        const isOptional = getFieldOptionality(context, field, isOneOf);
+
+        const args: ToAminoJSONMethod = {
+            context,
+            field,
+            isOptional
+        };
+
+        if (field.rule === 'repeated') {
+            switch (field.type) {
+                case 'string':
+                    return [...m, toAminoJSON.array(args, arrayTypes.string())];
+                case 'bytes':
+                    return [...m, toAminoJSON.array(args, arrayTypes.bytes(args))];
+                case 'bool':
+                    return [...m, toAminoJSON.array(args, arrayTypes.bool())];
+                case 'double':
+                    return [...m, toAminoJSON.array(args, arrayTypes.double())];
+                case 'float':
+                    return [...m, toAminoJSON.array(args, arrayTypes.float())];
+                case 'int32':
+                    return [...m, toAminoJSON.array(args, arrayTypes.int32())];
+                case 'sint32':
+                    return [...m, toAminoJSON.array(args, arrayTypes.sint32())];
+                case 'uint32':
+                    return [...m, toAminoJSON.array(args, arrayTypes.uint32())];
+                case 'fixed32':
+                    return [...m, toAminoJSON.array(args, arrayTypes.fixed32())];
+                case 'sfixed32':
+                    return [...m, toAminoJSON.array(args, arrayTypes.sfixed32())];
+                case 'int64':
+                    return [...m, toAminoJSON.array(args, arrayTypes.int64(args))];
+                case 'sint64':
+                    return [...m, toAminoJSON.array(args, arrayTypes.sint64(args))];
+                case 'uint64':
+                    return [...m, toAminoJSON.array(args, arrayTypes.uint64(args))];
+                case 'fixed64':
+                    return [...m, toAminoJSON.array(args, arrayTypes.fixed64(args))];
+                case 'sfixed64':
+                    return [...m, toAminoJSON.array(args, arrayTypes.sfixed64(args))];
+                default:
+                    switch (field.parsedType.type) {
+                        case 'Enum':
+                            return [...m, toAminoJSON.array(args, arrayTypes.enum(args))];
+                        case 'Type':
+                            return [...m, toAminoJSON.array(args, arrayTypes.type(args))];
+                    }
+                    return needsImplementation(fieldName, field);
+            }
+
+        }
+
+        if (field.keyType) {
+            switch (field.keyType) {
+                case 'string':
+                case 'int32':
+                case 'sint32':
+                case 'uint32':
+                case 'fixed32':
+                case 'sfixed32':
+                case 'int64':
+                case 'sint64':
+                case 'uint64':
+                case 'fixed64':
+                case 'sfixed64':
+                    return [...m, ...toAminoJSON.keyHash(args)];
+                default:
+                    return needsImplementation(fieldName, field);
+            }
+        }
+
+        switch (field.type) {
+            case 'string':
+                return [...m, toAminoJSON.string(args)];
+            case 'double':
+                return [...m, toAminoJSON.double(args)];
+            case 'float':
+                return [...m, toAminoJSON.float(args)];
+            case 'bytes':
+                return [...m, toAminoJSON.bytes(args)];
+            case 'bool':
+                return [...m, toAminoJSON.bool(args)];
+            case 'int32':
+                return [...m, toAminoJSON.int32(args)];
+            case 'sint32':
+                return [...m, toAminoJSON.sint32(args)];
+            case 'uint32':
+                return [...m, toAminoJSON.uint32(args)];
+            case 'fixed32':
+                return [...m, toAminoJSON.fixed32(args)];
+            case 'sfixed32':
+                return [...m, toAminoJSON.sfixed32(args)];
+            case 'int64':
+                return [...m, toAminoJSON.int64(args)];
+            case 'sint64':
+                return [...m, toAminoJSON.sint64(args)];
+            case 'uint64':
+                return [...m, toAminoJSON.uint64(args)];
+            case 'fixed64':
+                return [...m, toAminoJSON.fixed64(args)];
+            case 'sfixed64':
+                return [...m, toAminoJSON.sfixed64(args)];
+            case 'google.protobuf.Duration':
+            case 'Duration':
+                return [...m, toAminoJSON.duration(args)];
+            case 'google.protobuf.Timestamp':
+            case 'Timestamp':
+                return [...m, toAminoJSON.timestamp(args)];
+            default:
+                switch (field.parsedType.type) {
+                    case 'Enum':
+                        return [...m, toAminoJSON.enum(args)];
+                    case 'Type':
+                        return [...m, toAminoJSON.type(args)];
+                }
+                return needsImplementation(fieldName, field);
+        }
+    }, []);
+    return fields;
+};
+
+export const toAminoJSONMethod = (context: ProtoParseContext, name: string, proto: ProtoType) => {
+    const fields = toAminoJSONMethodFields(context, name, proto);
+    let varName = 'message';
+    if (!fields.length) {
+        varName = '_';
+    }
+
+    const SDKTypeName =
+        [name, 'SDKType']
+            .filter(Boolean).join('')
+
+
+    return objectMethod('method',
+        t.identifier('toAmino'),
+        [
+            identifier(
+                varName,
+                t.tsTypeAnnotation(
+                    t.tsTypeReference(
+                        t.identifier(name)
+                    )
+                )
+            )
+        ],
+        t.blockStatement([
+            t.variableDeclaration(
+                'const',
+                [
+                    t.variableDeclarator(
+                        identifier('obj', t.tsTypeAnnotation(t.tsAnyKeyword())),
+                        t.objectExpression([])
+                    )
+                ]
+            ),
+
+            ...fields,
+
+            // RETURN 
+            t.returnStatement(t.identifier('obj'))
+
+        ]),
+        false,
+        false,
+        false,
+        t.tsTypeAnnotation(
+            t.tsTypeReference(
+                t.identifier(SDKTypeName)
+            )
+        )
+    );
+};
---
+++ packages/ast/src/encoding/proto/to-amino/utils.ts
@@ -0,0 +1,397 @@
+import * as t from '@babel/types';
+import { getFieldNames } from '../../types';
+import { ToAminoJSONMethod } from './index';
+
+const notUndefinedSetValue = (sdkName: string, msgName: string, expr: t.Expression) => {
+    return t.expressionStatement(
+        t.logicalExpression(
+            '&&',
+            t.binaryExpression(
+                '!==',
+                t.memberExpression(
+                    t.identifier('message'),
+                    t.identifier(msgName)
+                ),
+                t.identifier('undefined')
+            ),
+            t.assignmentExpression(
+                '=',
+                t.memberExpression(
+                    t.identifier('obj'),
+                    t.identifier(sdkName)
+                ),
+                expr
+            )
+        )
+    );
+}
+
+export const toAminoJSON = {
+
+    scalar(args: ToAminoJSONMethod) {
+        const { propName, origName } = getFieldNames(args.field);
+
+        return notUndefinedSetValue(origName, propName, t.memberExpression(
+            t.identifier('message'),
+            t.identifier(propName)
+        ));
+    },
+
+    string(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    double(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    float(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    bool(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    number(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    int32(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    uint32(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    sint32(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    fixed32(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    sfixed32(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    long(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    int64(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    uint64(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    sint64(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    fixed64(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+    sfixed64(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    type(args: ToAminoJSONMethod) {
+        const { propName, origName } = getFieldNames(args.field);
+        const name = args.context.getTypeName(args.field);
+
+        // TODO isn't the nested conditional a waste? (using ts-proto as reference)
+        // maybe null is OK?
+        return notUndefinedSetValue(origName, propName, t.conditionalExpression(
+            t.memberExpression(
+                t.identifier('message'),
+                t.identifier(propName)
+            ),
+            t.callExpression(
+                t.memberExpression(
+                    t.identifier(name),
+                    t.identifier('toAmino')
+                ),
+                [
+                    t.memberExpression(
+                        t.identifier('message'),
+                        t.identifier(propName)
+                    )
+                ]
+            ),
+            t.identifier('undefined')
+        ));
+    },
+
+    enum(args: ToAminoJSONMethod) {
+        const { propName, origName } = getFieldNames(args.field);
+
+        const enumFuncName = args.context.getToEnum(args.field);
+        return notUndefinedSetValue(origName, propName, t.callExpression(
+            t.identifier(enumFuncName),
+            [
+                t.memberExpression(
+                    t.identifier('message'),
+                    t.identifier(propName)
+                )
+            ]
+        ));
+    },
+
+    bytes(args: ToAminoJSONMethod) {
+        return toAminoJSON.scalar(args);
+    },
+
+    duration(args: ToAminoJSONMethod) {
+        return toAminoJSON.type(args);
+    },
+
+    timestamp(args: ToAminoJSONMethod) {
+        return toAminoJSON.type(args);
+    },
+
+    keyHash(args: ToAminoJSONMethod) {
+
+        const { propName, origName } = getFieldNames(args.field);
+        const keyType = args.field.keyType;
+        const valueType = args.field.parsedType.name;
+
+        let toAminoJSON = null;
+        switch (valueType) {
+            case 'string':
+                toAminoJSON = t.identifier('v')
+                break;
+            case 'uint32':
+            case 'int32':
+                toAminoJSON = t.callExpression(
+                    t.memberExpression(
+                        t.identifier('Math'),
+                        t.identifier('round')
+                    ),
+                    [
+                        t.identifier('v')
+                    ]
+                )
+                break;
+            case 'int64':
+            case 'uint64':
+                toAminoJSON = t.callExpression(
+                    t.memberExpression(
+                        t.identifier('v'),
+                        t.identifier('toString')
+                    ),
+                    []
+                )
+                break;
+            default:
+                toAminoJSON = t.callExpression(
+                    t.memberExpression(
+                        t.identifier(valueType),
+                        t.identifier('toAmino')
+                    ),
+                    [
+                        t.identifier('v')
+                    ]
+                )
+        }
+
+
+        return [
+            t.expressionStatement(
+                t.assignmentExpression(
+                    '=',
+                    t.memberExpression(
+                        t.identifier('obj'),
+                        t.identifier(origName)
+                    ),
+                    t.objectExpression([])
+                )
+            ),
+            //
+            t.ifStatement(
+                t.memberExpression(
+                    t.identifier('message'),
+                    t.identifier(propName)
+                ),
+                t.blockStatement([
+                    t.expressionStatement(
+                        t.callExpression(
+                            t.memberExpression(
+                                t.callExpression(
+                                    t.memberExpression(
+                                        t.identifier('Object'),
+                                        t.identifier('entries')
+                                    ),
+                                    [
+                                        t.memberExpression(
+                                            t.identifier('message'),
+                                            t.identifier(propName)
+                                        )
+                                    ]
+                                ),
+                                t.identifier('forEach')
+                            ),
+                            [
+                                t.arrowFunctionExpression(
+                                    [
+                                        t.arrayPattern(
+                                            [
+                                                t.identifier('k'),
+                                                t.identifier('v')
+                                            ]
+                                        )
+                                    ],
+                                    t.blockStatement([
+                                        t.expressionStatement(
+                                            t.assignmentExpression(
+                                                '=',
+                                                t.memberExpression(
+                                                    t.memberExpression(
+                                                        t.identifier('obj'),
+                                                        t.identifier(origName)
+                                                    ),
+                                                    t.identifier('k'),
+                                                    true
+                                                ),
+                                                toAminoJSON
+                                            )
+                                        )
+                                    ])
+                                )
+                            ]
+                        )
+                    )
+                ])
+            )
+        ]
+    },
+
+    array(args: ToAminoJSONMethod, expr: t.Expression) {
+        const { propName, origName } = getFieldNames(args.field);
+
+        return t.ifStatement(
+            t.memberExpression(
+                t.identifier('message'),
+                t.identifier(propName)
+            ),
+            t.blockStatement([
+                t.expressionStatement(
+                    t.assignmentExpression(
+                        '=',
+                        t.memberExpression(
+                            t.identifier('obj'),
+                            t.identifier(origName)
+                        ),
+                        t.callExpression(
+                            t.memberExpression(
+                                t.memberExpression(
+                                    t.identifier('message'),
+                                    t.identifier(propName)
+                                ),
+                                t.identifier('map')
+                            ),
+                            [
+                                t.arrowFunctionExpression(
+                                    [
+                                        t.identifier('e')
+                                    ],
+                                    expr
+                                )
+                            ]
+                        )
+                    )
+                )
+            ]),
+            t.blockStatement([
+                t.expressionStatement(
+                    t.assignmentExpression(
+                        '=',
+                        t.memberExpression(
+                            t.identifier('obj'),
+                            t.identifier(origName)
+                        ),
+                        t.arrayExpression([])
+                    )
+                )
+            ])
+        );
+    }
+
+};
+
+export const arrayTypes = {
+    scalar() {
+        return t.identifier('e');
+    },
+    string() {
+        return arrayTypes.scalar();
+    },
+    double() {
+        return arrayTypes.scalar();
+    },
+    float() {
+        return arrayTypes.scalar();
+    },
+    bool() {
+        return arrayTypes.scalar();
+    },
+    number() {
+        return arrayTypes.scalar();
+    },
+    int32() {
+        return arrayTypes.number();
+    },
+    uint32() {
+        return arrayTypes.number();
+    },
+    sint32() {
+        return arrayTypes.number();
+    },
+    fixed32() {
+        return arrayTypes.number();
+    },
+    sfixed32() {
+        return arrayTypes.number();
+    },
+    long(args: ToAminoJSONMethod) {
+        return arrayTypes.scalar();
+    },
+    int64(args: ToAminoJSONMethod) {
+        return arrayTypes.long(args);
+    },
+    uint64(args: ToAminoJSONMethod) {
+        return arrayTypes.long(args);
+    },
+    sint64(args: ToAminoJSONMethod) {
+        return arrayTypes.long(args);
+    },
+    fixed64(args: ToAminoJSONMethod) {
+        return arrayTypes.long(args);
+    },
+    sfixed64(args: ToAminoJSONMethod) {
+        return arrayTypes.long(args);
+    },
+    bytes(args: ToAminoJSONMethod) {
+        return arrayTypes.scalar();
+    },
+    enum(args: ToAminoJSONMethod) {
+        const enumFuncName = args.context.getToEnum(args.field);
+        return t.callExpression(
+            t.identifier(enumFuncName),
+            [
+                t.identifier('e')
+            ]
+        );
+    },
+    type(args: ToAminoJSONMethod) {
+        const name = args.context.getTypeName(args.field);
+        return t.conditionalExpression(
+            t.identifier('e'),
+            t.callExpression(
+                t.memberExpression(
+                    t.identifier(name),
+                    t.identifier('toAmino')
+                ),
+                [
+                    t.identifier('e')
+                ]
+            ),
+            t.identifier('undefined')
+        );
+    }
+}
+
