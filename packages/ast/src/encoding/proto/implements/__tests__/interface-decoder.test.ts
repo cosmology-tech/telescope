5c1e5aa1f86b484cfc146055edf0323f167c2370
Dan Lynch <pyramation@gmail.com>
2022-12-10 18:38:17 +0800

    test

--- packages/ast/src/encoding/proto/implements/__tests__/interface-decoder.test.ts
+++ packages/ast/src/encoding/proto/implements/__tests__/interface-decoder.test.ts
@@ -1,17 +1,25 @@
 import { getNestedProto } from '@osmonauts/proto-parser';
-import { defaultTelescopeOptions } from '@osmonauts/types';
 import { expectCode, getTestProtoStore, printCode } from '../../../../../test-utils/'
 import { ProtoParseContext } from '../../../context';
 import { createInterfaceDecoder } from '../decoder';
 
 const store = getTestProtoStore();
+store.options.prototypes.implementsAcceptsAny = true;
 store.traverseAll();
 
 describe('osmosis/gamm/v1beta1/tx.proto', () => {
-    const ref = store.findProto('osmosis/gamm/v1beta1/tx.proto');
-    const context = new ProtoParseContext(ref, store, defaultTelescopeOptions);
-    it('MsgJoinPool', () => {
+    const ref = store.findProto('osmosis/gamm/v1beta1/query.proto');
+    const context = new ProtoParseContext(ref, store, store.options);
+    it('PoolI', () => {
         // getNestedProto(ref.traversed).MsgJoinPool)
+        console.log(ref.traversed);
+        expect(ref.traversed.acceptsInterface).toEqual({
+            PoolI: [
+                'QueryPoolResponse',
+                'QueryPoolsResponse',
+                'QueryPoolsWithFilterResponse'
+            ]
+        })
         expectCode(createInterfaceDecoder(
             context,
             'PoolI_InterfaceDecoder',
