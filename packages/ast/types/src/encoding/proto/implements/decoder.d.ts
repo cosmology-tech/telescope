e28d2909ea89d09c7b15c88028aa190a2d1db82f
Dan Lynch <pyramation@gmail.com>
2022-12-10 20:56:05 +0800

    export interface decoder

--- packages/ast/types/src/encoding/proto/implements/decoder.d.ts
+++ packages/ast/types/src/encoding/proto/implements/decoder.d.ts
@@ -1,3 +1,5 @@
 import * as t from '@babel/types';
+import { ProtoRef } from '@osmonauts/types';
 import { ProtoParseContext } from "../../context";
-export declare const createInterfaceDecoder: (context: ProtoParseContext, decoder: string, typeHash: Record<string, string>) => t.ExportNamedDeclaration;
+export declare const createInterfaceDecoder: (context: ProtoParseContext, ref: ProtoRef, interfaceName: string) => t.ExportNamedDeclaration;
+export declare const createInterfaceDecoderHelper: (context: ProtoParseContext, decoder: string, typeHash: Record<string, string>) => t.ExportNamedDeclaration;
