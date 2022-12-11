dfe794b453e1fdbc42cfc2d04cc6221732033ae1
Dan Lynch <pyramation@gmail.com>
2022-12-11 17:51:38 +1000

    types

---
+++ packages/ast/types/src/encoding/proto/implements/from-amino.d.ts
@@ -0,0 +1,4 @@
+import * as t from '@babel/types';
+import { TraverseTypeUrlRef } from '@osmonauts/types';
+import { ProtoParseContext } from "../../context";
+export declare const createInterfaceFromAminoHelper: (context: ProtoParseContext, functionName: string, typeRefs: TraverseTypeUrlRef[]) => t.ExportNamedDeclaration;
---
+++ packages/ast/types/src/encoding/proto/implements/to-amino.d.ts
@@ -0,0 +1,3 @@
+import * as t from '@babel/types';
+import { ProtoParseContext } from "../../context";
+export declare const createInterfaceToAminoHelper: (context: ProtoParseContext, functionName: string) => t.ExportNamedDeclaration;
