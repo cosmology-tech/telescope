13c92634105b0f3c40985140639c3528d6b58aa2
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-11 05:34:27 +0700

    docs for test-ast.js

--- packages/ast/scripts/test-ast.js
+++ packages/ast/scripts/test-ast.js
@@ -1,3 +1,9 @@
+// This file is a demonstation of how ast package works. In order to try it yourself you need:
+// 1. Create a 'fixture.tx' file in the same directory as this file and write any TypeScript to it.
+// 2. cd into `packages/ast`.
+// 3. Run `yarn test:ast`.
+// `test-output.json` will be created containing an abstract syntax tree of the `fixture.ts` file.
+
 import { stringify } from 'ast-stringify';
 import { parse } from '@babel/parser';
 import { readFileSync, writeFileSync } from 'fs';
