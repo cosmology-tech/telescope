589f89d8fc5727fe00113769993c32bcf227d2ae
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-11 04:56:05 +0700

    scaff grpc-web

---
+++ packages/ast/src/clients/rpc/class/grpc-gateway.ts
@@ -0,0 +1,10 @@
+import { GenericParseContext } from '../../../encoding';
+import { ProtoService, ProtoServiceMethod } from '@osmonauts/types';
+
+
+export const createGRPCGatewatClient = (
+    context: GenericParseContext,
+    service: ProtoService
+) => {
+    
+}
