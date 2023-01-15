077b5e4b1d8d92beffeddc99e329d7c552e081dd
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-15 17:57:00 +0700

    some moves

--- packages/ast/types/src/clients/rpc/class/grpc-gateway/index.d.ts
+++ packages/ast/types/src/clients/rpc/class/grpc-gateway/index.d.ts
@@ -1,2 +1,1 @@
 export * from './grpc-gateway';
-export * from './tendermint';
--- packages/ast/types/src/clients/rpc/class/tendermint/rpc.d.ts
+++ packages/ast/types/src/clients/rpc/class/tendermint/rpc.d.ts
@@ -1,6 +1,6 @@
 import * as t from '@babel/types';
 import { ProtoService } from '@osmonauts/types';
-import { GenericParseContext } from '../../../encoding';
+import { GenericParseContext } from '../../../../encoding';
 export declare const createRpcClientInterface: (context: GenericParseContext, service: ProtoService) => t.ExportNamedDeclaration;
 export declare const getRpcClassName: (service: ProtoService) => string;
 export declare const createRpcClientClass: (context: GenericParseContext, service: ProtoService) => t.ExportNamedDeclaration;
