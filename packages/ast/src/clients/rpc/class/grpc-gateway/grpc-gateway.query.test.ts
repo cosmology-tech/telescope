74b608b99b3579bcb67599ee20e90c6a22b38ad7
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-15 20:40:01 +0700

    grpc-gateway query test

---
+++ packages/ast/src/clients/rpc/class/grpc-gateway/grpc-gateway.query.test.ts
@@ -0,0 +1,16 @@
+import { ProtoStore, traverse, getNestedProto } from '@osmonauts/proto-parser'
+import { defaultTelescopeOptions, ProtoService } from '@osmonauts/types';
+import { expectCode, getTestProtoStore, printCode } from '../../../../../test-utils';
+import { GenericParseContext } from '../../../../encoding';
+import { createGRPCGatewayQueryClass } from './grpc-gateway.query';
+
+const store = getTestProtoStore();
+store.traverseAll();
+
+it('GRPC-Gateway Msg Client', () => {
+    const ref = store.findProto('cosmos/bank/v1beta1/tx.proto');
+    const res = traverse(store, ref);
+    const service: ProtoService = getNestedProto(res).Msg;
+    const context = new GenericParseContext(ref, store, store.options);
+    printCode(createGRPCGatewayQueryClass(context, service))
+});
