e182cb95b8d9717cb55bc5f91349799f5a2c00da
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-15 20:14:54 +0700

    add grpc-gateway query file

---
+++ packages/ast/src/clients/rpc/class/grpc-gateway/grpc-gateway.query.ts
@@ -0,0 +1,8 @@
+import { GenericParseContext } from '../../../../encoding';
+import { ProtoService, ProtoServiceMethod } from '@osmonauts/types';
+import { arrowFunctionExpression, classDeclaration, classMethod, classProperty, commentBlock, identifier, tsMethodSignature } from '../../../../utils';
+import { camel } from '@osmonauts/utils';
+import { returnReponseType } from '../utils/rpc';
+
+import * as t from '@babel/types'
+
