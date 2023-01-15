9f15de448cccf15ea10ffe54e28eb4f8c931672e
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-15 20:16:32 +0700

    delete file

--- packages/ast/types/src/clients/rpc/class/grpc-gateway/grpc-gateway.d.ts
+++
@@ -1,4 +0,0 @@
-import { GenericParseContext } from '../../../../encoding';
-import { ProtoService } from '@osmonauts/types';
-import * as t from '@babel/types';
-export declare const createGRPCGatewayClass: (context: GenericParseContext, service: ProtoService) => t.ExportNamedDeclaration;
