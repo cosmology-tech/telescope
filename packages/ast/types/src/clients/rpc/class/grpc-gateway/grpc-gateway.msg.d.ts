36fb5a33e9b91f6b8d40e19872e80468b1e7b8d9
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-15 20:17:05 +0700

    wip

---
+++ packages/ast/types/src/clients/rpc/class/grpc-gateway/grpc-gateway.msg.d.ts
@@ -0,0 +1,4 @@
+import { GenericParseContext } from '../../../../encoding';
+import { ProtoService } from '@osmonauts/types';
+import * as t from '@babel/types';
+export declare const createGRPCGatewayMsgClass: (context: GenericParseContext, service: ProtoService) => t.ExportNamedDeclaration;
