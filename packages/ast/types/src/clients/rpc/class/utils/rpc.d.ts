f427622b22aec722eb1d0a6c9fc15a745c70afbd
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-01-20 16:34:47 +0700

    wip

--- packages/ast/types/src/clients/rpc/class/utils/rpc.d.ts
+++ packages/ast/types/src/clients/rpc/class/utils/rpc.d.ts
@@ -3,3 +3,4 @@ import * as t from '@babel/types';
 export declare const processRpcComment: (e: ProtoServiceMethod) => string;
 export declare const cleanType: (ResponseType: string) => string;
 export declare const returnReponseType: (ResponseType: string) => t.TSTypeAnnotation;
+export declare const optionalBool: (hasParams: boolean, fieldNames: string[]) => boolean;
