49efc20cd401ea90101f8d03f8b09ea9f4f3b7ac
hoangdv2429 <hoangdv2429@gmail.com>
2023-01-31 13:26:48 +0700

    adding interface for msg

--- packages/ast/src/clients/rpc/class/grpc-web/utils/grpc-web.ts
+++ packages/ast/src/clients/rpc/class/grpc-web/utils/grpc-web.ts
@@ -1,6 +1,7 @@
 import { identifier } from '../../../../../utils';
 import * as t from '@babel/types'
 import { ProtoService } from '@osmonauts/types';
+import { GenericParseContext } from '../../../../../encoding';
 
 // second params for grpc-web method
 export const metadata = identifier(
@@ -48,4 +49,76 @@ export const makeComment = (comment: string) => {
 
 export const getRpcClassName = (service: ProtoService) => {
     return `${service.name}ClientImpl`;
+}
+
+export const getProtoRequest = (methodName: string, context: GenericParseContext, service: ProtoService) => {
+
+        const interfaceRequestName = 'Msg' + methodName;
+        
+        // get all the params of said methodName into an array
+        const methodFields = Object.keys(service.methods[methodName].fields)
+        // console.log(methodFields);
+        let methodFieldsType = methodFields.map(key => {
+            return  service.methods[methodName].fields[key].type
+        })
+        // console.log(methodFieldsType);
+        
+
+        const methods = methodFields
+        .map(key => {
+            console.log(key);
+            let typeOfProperties = service.methods[methodName].fields[key].type;
+            if(typeOfProperties.includes(context.ref.proto.package)){
+                //TO-DO: clean up and add import for required type 
+                //TO-DO: add dynamic for not only msg type but also query type
+                // context.addUtil 
+                const cleanType = typeOfProperties.split('.');
+                typeOfProperties =  cleanType[cleanType.length - 1]
+            }
+            
+            return  t.tsPropertySignature(
+                    t.identifier(key),
+                    t.tsTypeAnnotation(
+                        t.tsTypeReference(
+                                t.identifier(typeOfProperties)
+                            )
+                    )
+                )
+            }
+        )
+            
+        // });
+    
+    return t.exportNamedDeclaration(
+        t.tsInterfaceDeclaration(
+            t.identifier(interfaceRequestName),
+            null,
+            null,
+            t.tsInterfaceBody(
+                [
+                    // t.tsPropertySignature(
+                    //     t.identifier('granter'),
+                    //     t.tsTypeAnnotation(
+                    //         t.tsStringKeyword()
+                    //     )
+                    // ),
+                    // t.tsPropertySignature(
+                    //     t.identifier('grantee'),
+                    //     t.tsTypeAnnotation(
+                    //         t.tsStringKeyword()
+                    //     )
+                    // ),
+                    // t.tsPropertySignature(
+                    //     t.identifier('grant'),
+                    //     t.tsTypeAnnotation(
+                    //         t.tsTypeReference(
+                    //             t.identifier('Grant')
+                    //         )
+                    //     )                        
+                    // )
+                    ...methods
+                ]
+            )
+        )
+    )
 }
