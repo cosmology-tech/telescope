b256f69fad621c46ab3f060b3481c10b07d8a74e
hoangdv2429 <hoangdv2429@gmail.com>
2023-02-04 05:12:17 +0700

    draft function for getting methodDesc

--- packages/ast/src/clients/rpc/class/grpc-web/utils/grpc-web.ts
+++ packages/ast/src/clients/rpc/class/grpc-web/utils/grpc-web.ts
@@ -51,19 +51,21 @@ export const getRpcClassName = (service: ProtoService) => {
     return `${service.name}ClientImpl`;
 }
 
-export const GetQueryDesc = (context: GenericParseContext) => {
-    // return `${context.ref.proto.package}.Query`; 
+export const GetDesc = (context: GenericParseContext, service: ProtoService) => {
+    const descName = service.name + 'Desc'
+    const serviceName = context.ref.proto.package + '.' + service.name
+
     return t.exportNamedDeclaration(
         t.variableDeclaration(
             'const',
             [
                 t.variableDeclarator(
-                t.identifier('QueryDesc'),
+                t.identifier(descName),
                 t.objectExpression(
                     [ 
                         t.objectProperty(
                             t.identifier('serviceName'),
-                            t.stringLiteral(`${context.ref.proto.package}.Query`)
+                            t.stringLiteral(serviceName)
                         )
                     ]                 
                     )
@@ -73,3 +75,138 @@ export const GetQueryDesc = (context: GenericParseContext) => {
     )
 }
 
+export const getMethodDesc = (
+    context: GenericParseContext, service: ProtoService, methodName: string
+) => {
+    return t.exportNamedDeclaration(
+        t.variableDeclaration(
+            'const',
+            [
+                t.variableDeclarator(
+                    t.identifier(service.name + methodName + 'Desc: UnaryMethodDefinitionish'),
+                    t.objectExpression(
+                        [
+                            t.objectProperty(
+                                t.identifier('methodName'),
+                                t.stringLiteral(methodName)
+                            ),
+                            t.objectProperty(
+                                t.identifier('service'),
+                                t.identifier(service.name + 'Desc')
+                            ),
+                            t.objectProperty(
+                                t.identifier('requestStream'),
+                                t.booleanLiteral(false)
+                            ),
+                            t.objectProperty(
+                                t.identifier('reponseStream'),
+                                t.booleanLiteral(false)
+                            ),
+                            t.objectProperty(
+                                t.identifier('requestType'),
+                                t.tsAsExpression(
+                                    t.objectExpression(
+                                        [
+                                            t.objectMethod(
+                                                'method',
+                                                t.identifier('serializeBinary'),
+                                                [],
+                                                t.blockStatement(
+                                                    [
+                                                    t.returnStatement(
+                                                        t.callExpression(
+                                                            t.memberExpression(
+                                                                t.callExpression(
+                                                                    t.memberExpression(
+                                                                        t.identifier('QueryGrantsRequest'),
+                                                                        t.identifier('encode')
+                                                                    ),
+                                                                    [
+                                                                        t.thisExpression()
+                                                                    ]
+                                                                ),
+                                                                t.identifier('finish')
+                                                            ),
+                                                            []
+                                                        )
+                                                    )
+                                                    ]
+                                                )
+                                            )
+                                        ]
+                                    ),        
+                                    t.tsAnyKeyword()                                  
+                                )
+                            ),
+                            t.objectProperty(
+                                t.identifier('responseType'),
+                                t.tsAsExpression(
+                                    t.objectExpression(
+                                        [
+                                            t.objectMethod(
+                                                'method',
+                                                t.identifier('deserializeBinary'),
+                                                [
+                                                    identifier('data', t.tsTypeAnnotation(
+                                                        t.tsTypeReference(
+                                                            t.identifier('Uint8Array')
+                                                        )
+                                                    ))
+                                                ],
+                                                t.blockStatement(
+                                                    [
+                                                    t.returnStatement(
+                                                        t.objectExpression(
+                                                            [
+                                                            t.spreadElement(
+                                                                t.callExpression(
+                                                                    t.memberExpression(
+                                                                        t.identifier('QueryGrantsResponse'),
+                                                                        t.identifier('decode')
+                                                                    ),
+                                                                    [t.identifier('data')]
+                                                                )
+                                                            )
+                                                            ]
+                                                        )
+                                                    )
+                                                    ]
+                                                )
+                                            )
+                                        ]
+                                    ),        
+                                    t.tsAnyKeyword()                                  
+                                )
+                            ),
+                        ]
+                    )
+                )
+            ]
+        )
+    ) 
+}
+
+
+/*
+export const QueryGrantsDesc: UnaryMethodDefinitionish = {
+  methodName: "Grants",
+  service: QueryDesc,
+  requestStream: false,
+  responseStream: false,
+  requestType: {
+    serializeBinary() {
+      return QueryGrantsRequest.encode(this).finish();
+    },
+  } as any,
+  responseType: {
+    deserializeBinary(data: Uint8Array) {
+      return {
+        ...QueryGrantsResponse.decode(data),
+        toObject() {
+          return this;
+        },
+      };
+    },
+  } as any,
+};
+*/
