e85c5a166eb86fafe5aaf17ab7e748a977151c66
hoangdv2429 <hoangdv2429@gmail.com>
2023-05-16 20:21:54 +0700

    refactor

--- packages/ast/types/src/clients/rpc/scoped/grpc-web.d.ts
+++ packages/ast/types/src/clients/rpc/scoped/grpc-web.d.ts
@@ -1,5 +1,6 @@
 import * as t from '@babel/types';
 import { GenericParseContext } from '../../../encoding';
+export declare const grpcScaffold: () => t.Statement[];
 export declare const grpcWebNewAwaitImport: (path: string, className: string) => t.NewExpression;
 export declare const grpcNestedImportObject: (obj: object, className: string) => any;
 export declare const createScopedGrpcWebFactory: (context: GenericParseContext, obj: object, identifier: string) => t.ExportNamedDeclaration;
