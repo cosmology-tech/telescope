779b3aa49dba951f9980dc1aa49d01af2761d34a
Zetazzz <zetazz@163.com>
2023-08-03 18:47:16 +0800

    add extraKey to useRPCEndpoint

--- packages/telescope/src/helpers/react-query.ts
+++ packages/telescope/src/helpers/react-query.ts
@@ -62,3 +62,76 @@ export const useTendermintClient = ({
     return { client }
 };
 `;
+
+export const reactQueryVNext = `import { getRpcClient } from './extern'
+import {
+    useQuery,
+    UseQueryOptions,
+} from '@tanstack/react-query';
+
+import { HttpEndpoint, ProtobufRpcClient } from '@cosmjs/stargate';
+import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+
+export interface ReactQueryParams<TResponse, TData = TResponse> {
+    options?: UseQueryOptions<TResponse, Error, TData>;
+}
+
+export interface UseRpcClientQuery<TData> extends ReactQueryParams<ProtobufRpcClient, TData> {
+    rpcEndpoint: string | HttpEndpoint;
+}
+
+export interface UseRpcEndpointQuery<TData> extends ReactQueryParams<string | HttpEndpoint, TData> {
+  getter: () => Promise<string | HttpEndpoint>;
+  extraKey?: string
+}
+
+export const useRpcEndpoint = <TData = string | HttpEndpoint>({
+  getter,
+  options,
+  extraKey
+}: UseRpcEndpointQuery<TData>) => {
+  const keys = ['rpcEndpoint', getter];
+
+  if(extraKey){
+    keys.push(extraKey);
+  }
+
+  return useQuery<string | HttpEndpoint, Error, TData>(keys, async () => {
+      return await getter();
+  }, options);
+};
+
+export const useRpcClient = <TData = ProtobufRpcClient>({
+    rpcEndpoint,
+    options,
+}: UseRpcClientQuery<TData>) => {
+    return useQuery<ProtobufRpcClient, Error, TData>(['rpcClient', rpcEndpoint], async () => {
+        return await getRpcClient(rpcEndpoint);
+    }, options);
+};
+
+interface UseTendermintClient extends ReactQueryParams<Tendermint34Client> {
+    rpcEndpoint: string | HttpEndpoint;
+}
+
+/**
+ * Hook that uses react-query to cache a connected tendermint client.
+ */
+export const useTendermintClient = ({
+    rpcEndpoint,
+    options,
+}: UseTendermintClient) => {
+    const { data: client } = useQuery<Tendermint34Client, Error, Tendermint34Client>(
+        ['client', 'tendermint', rpcEndpoint],
+        () => Tendermint34Client.connect(rpcEndpoint),
+        {
+            // allow overriding
+            onError: (e) => {
+                throw new Error(\`Failed to connect to \${rpcEndpoint}\` + '\\n' + e)
+            },
+            ...options,
+        }
+    )
+    return { client }
+};
+`;
