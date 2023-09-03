9699fa60dff3ec882e909cf923cf3f62f3b2388e
Zetazzz <zetazz@163.com>
2023-09-03 13:47:50 +0800

    remove babel from starship

--- packages/starship/scripts/codegen.js
+++ packages/starship/scripts/codegen.js
@@ -11,14 +11,62 @@ telescope({
   outPath,
   options: {
     removeUnusedImports: true,
+    tsDisable: {
+      patterns: ['**/tx.registry.ts'],
+      files: [
+        'cosmos/auth/v1beta1/query.ts',
+        'cosmos/gov/v1/tx.ts',
+        'cosmos/gov/v1beta1/gov.ts',
+        'cosmos/gov/v1beta1/tx.ts',
+
+        'cosmos/authz/v1beta1/authz.ts',
+        'cosmos/authz/v1beta1/tx.ts',
+        'cosmos/bundle.ts',
+        'cosmos/staking/v1beta1/staking.ts',
+        'cosmos/staking/v1beta1/tx.ts'
+      ]
+    },
     interfaces: {
       enabled: true,
-      useUnionTypes: false
+      useUnionTypes: true
     },
     prototypes: {
       addTypeUrlToDecoders: true,
       excluded: {
-        packages: ['cosmos.group.v1']
+        packages: [
+          'ibc.applications.fee.v1', // issue with parsing protos (LCD routes with nested objects in params)
+
+          'akash.**',
+          'cosmos.authz.**',
+
+          'cosmos.app.v1alpha1',
+          'cosmos.app.v1beta1',
+          'cosmos.base.kv.v1beta1',
+          'cosmos.base.reflection.v1beta1',
+          'cosmos.base.snapshots.v1beta1',
+          'cosmos.base.store.v1beta1',
+          'cosmos.base.tendermint.v1beta1',
+          'cosmos.crisis.v1beta1',
+          'cosmos.evidence.v1beta1',
+          'cosmos.feegrant.v1beta1',
+          'cosmos.genutil.v1beta1',
+          'cosmos.group.v1beta1',
+
+          'cosmos.autocli.v1',
+
+          'cosmos.group.v1',
+          'cosmos.msg.v1',
+          'cosmos.nft.v1beta1',
+          'cosmos.capability.v1beta1',
+          'cosmos.orm.v1alpha1',
+          'cosmos.orm.v1',
+          'cosmos.slashing.v1beta1',
+          'cosmos.vesting.v1beta1',
+          'google.api.**',
+          'google.logging.**',
+          'ibc.core.port.v1',
+          'ibc.core.types.v1'
+        ]
       },
       methods: {
         fromJSON: false,
@@ -50,6 +98,14 @@ telescope({
     packages: {
       osmosis: {
         classesUseArrowFunctions: true
+      },
+      cosmos: {
+        staking: {
+          interfaces: {
+            enabled: false,
+            useUnionTypes: false
+          }
+        }
       }
     }
   }
