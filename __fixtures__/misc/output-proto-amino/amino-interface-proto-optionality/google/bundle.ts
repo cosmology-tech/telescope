a350de27e44b3b015d3873bb3d4900cde64f47c9
Zetazzz <zetazz@163.com>
2023-12-14 10:15:20 +0800

    add useProtoOptionality option

---
+++ __fixtures__/misc/output-proto-amino/amino-interface-proto-optionality/google/bundle.ts
@@ -0,0 +1,20 @@
+import * as _78 from "./api/expr/v1alpha1/eval";
+import * as _79 from "./protobuf/any";
+import * as _80 from "./protobuf/duration";
+import * as _81 from "./protobuf/timestamp";
+import * as _82 from "./protobuf/descriptor";
+export namespace google {
+  export namespace api {
+    export namespace expr {
+      export const v1alpha1 = {
+        ..._78
+      };
+    }
+  }
+  export const protobuf = {
+    ..._79,
+    ..._80,
+    ..._81,
+    ..._82
+  };
+}
