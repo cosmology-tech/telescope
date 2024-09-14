b317d1f9dfa794d73dadb124047608d9e2b38f67
j-yw <32629001+j-yw@users.noreply.github.com>
2024-09-14 22:16:35 +0800

    chore: bundle new fixture outputs

---
+++ __fixtures__/misc/output-base64/gogoproto/bundle.ts
@@ -0,0 +1,4 @@
+import * as _184 from "./gogo";
+export const gogoproto = {
+  ..._184
+};
---
+++ __fixtures__/misc/output-base64/misc/bundle.ts
@@ -0,0 +1,20 @@
+import * as _190 from "./all_fields";
+import * as _191 from "./eval_request";
+import * as _192 from "./nest";
+import * as _193 from "./tx";
+import * as _194 from "./tx.amino";
+import * as _195 from "./tx.registry";
+import * as _196 from "./tx.rpc.msg";
+import * as _197 from "./rpc.tx";
+export const misc = {
+  ..._190,
+  ..._191,
+  ..._192,
+  ..._193,
+  ..._194,
+  ..._195,
+  ..._196,
+  ClientFactory: {
+    ..._197
+  }
+};
