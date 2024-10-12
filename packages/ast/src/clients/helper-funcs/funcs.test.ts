4fd6603df25861532c3003261610b058ff5bf6a4
Zetazzz <zetazz@163.com>
2024-10-12 19:48:08 +0800

    add ast test

--- packages/ast/src/clients/helper-funcs/funcs.test.ts
+++ packages/ast/src/clients/helper-funcs/funcs.test.ts
@@ -1,2 +1,6 @@
+import { expectCode } from "../../../test-utils";
+import { createMsgHelperCreator } from "./msg-funcs";
+
 it('works', async () => {
+  expectCode(createMsgHelperCreator({} as any, {} as any, 'balance', 'createGetBalance'));
 });
