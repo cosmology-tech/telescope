2cad946b890e2478370abddc6fce92165d5a360a
j-yw <32629001+j-yw@users.noreply.github.com>
2024-10-14 15:29:26 +0800

    feat: ast generation code for msg funcs and helper funcs.

--- packages/ast/src/clients/helper-funcs/msg-funcs.ts
+++ packages/ast/src/clients/helper-funcs/msg-funcs.ts
@@ -1,6 +1,15 @@
-import * as t from "@babel/types";
+import * as ast from "@babel/types";
 import { ProtoService } from "@cosmology/types";
 import { GenericParseContext } from "../../encoding";
+
+// ** The end result of generation should include the following imports
+// TODO: I need to add the imports for generation instead of hard coding them, it should comes from the UTILS in telescope
+/**
+    import { buildTx, ISigningClient } from "../../../helper-func-types";
+    import { MsgSend, MsgMultiSend } from "./tx";
+    import { toConverters, toEncoders } from "@interchainjs/cosmos/utils";
+    import { buildUseMutation } from "../../../react-query";
+ */
 
 /**
  *
@@ -13,16 +22,61 @@ import { GenericParseContext } from "../../encoding";
 export function createMsgHelperCreator(
     context: GenericParseContext,
     service: ProtoService,
-    methodKey: string,
-    helperCreatorName: string
+    methodKey?: string,
+    helperCreatorName?: string
 ) {
-    // get related encoders.
-    // e.g.: encoders: toEncoders(MsgMultiSend),
+    console.log(`🍀 \n | 🍄 file: msg-funcs.ts:19 \n | 🍄 service:`, service);
+    const createrFunctions = Object.keys(service.methods).map((method) => {
+        const callExpression = ast.callExpression(ast.identifier("buildTx"), [
+            ast.objectExpression([
+                ast.objectProperty(
+                    ast.identifier("getSigningClient"),
+                    ast.identifier("getSigningClient")
+                ),
+                ast.objectProperty(
+                    ast.identifier("typeUrl: "),
+                    ast.memberExpression(
+                        ast.identifier(`Msg${method}`),
+                        ast.identifier("typeUrl")
+                    )
+                ),
+                //**TODO: Improvements, where can i grab toEncoders and toConverters instead of hard coding the string?
+                //!!FIX: the toEncoders and toConverters are functions calls, not strings
+                ast.objectProperty(
+                    ast.identifier("encoders"),
+                    ast.stringLiteral(`toEncoders(Msg${method})`)
+                ),
+                ast.objectProperty(
+                    ast.identifier("converters"),
+                    ast.stringLiteral(`toConverters(Msg${method})`)
+                ),
+            ]),
+        ]);
 
-    // get related converters.
-    // e.g.: converters: toConverters(MsgMultiSend),
+        callExpression.typeParameters = ast.tsTypeParameterInstantiation([
+            ast.tsTypeReference(ast.identifier(`Msg${method}`)),
+        ]);
 
-    return t.returnStatement(t.identifier(helperCreatorName));
+        const customHookArgumentsType = ast.tsTypeAnnotation(
+            //TODO: Improvements, Figure out how to write ast code to generate the function expression below instead of hard coding the strong.
+            ast.tsTypeReference(
+                ast.identifier(" () => ISigningClient | undefined")
+            )
+        );
+        const arg = ast.identifier("getSigningClient");
+        arg.typeAnnotation = customHookArgumentsType;
+
+        const arrowFuncExp = ast.arrowFunctionExpression([arg], callExpression);
+
+        return ast.variableDeclaration("const", [
+            ast.variableDeclarator(
+                ast.identifier(`create${method}`),
+                arrowFuncExp
+            ),
+        ]);
+    });
+
+    return ast.program([...createrFunctions]);
 }
 
 /**
@@ -37,9 +91,35 @@ export function createMsgHelperCreator(
 export function createMsgHooks(
     context: GenericParseContext,
     service: ProtoService,
-    methodKey: string,
-    helperCreatorName: string,
-    hookName: string
+    methodKey?: string,
+    helperCreatorName?: string,
+    hookName?: string
 ) {
-    return t.returnStatement(t.identifier(hookName));
+    const hooks = Object.keys(service.methods).map((method) => {
+        const callExpression = ast.callExpression(
+            ast.identifier("buildUseMutation"),
+            [
+                ast.objectExpression([
+                    ast.objectProperty(
+                        ast.identifier("builderMutationFn"),
+                        ast.identifier(`create${method}`)
+                    ),
+                ]),
+            ]
+        );
+
+        callExpression.typeParameters = ast.tsTypeParameterInstantiation([
+            ast.tsTypeReference(ast.identifier(`Msg${method}`)),
+            ast.tsTypeReference(ast.identifier(`Error`)),
+        ]);
+
+        return ast.variableDeclaration("const", [
+            ast.variableDeclarator(
+                ast.identifier(`use${method}`),
+                callExpression
+            ),
+        ]);
+    });
+
+    return ast.program([...hooks]);
 }
--- packages/ast/src/clients/helper-funcs/query-funcs.ts
+++ packages/ast/src/clients/helper-funcs/query-funcs.ts
@@ -1,6 +1,15 @@
-import * as t from "@babel/types";
+import * as ast from "@babel/types";
 import { ProtoService } from "@cosmology/types";
 import { GenericParseContext } from "../../encoding";
+
+// ** The end result of generation should include the following imports
+// TODO: I need to add the imports for generation instead of hard coding them, it should comes from the UTILS in telescope
+/**
+    import { buildQuery } from "../../../helper-func-types";
+    import { Rpc } from "../../../helpers";
+    import { UseQueryParams, buildUseQuery } from "../../../react-query";
+    import { QueryBalanceRequest, QueryBalanceResponse, QueryAllBalancesRequest, QueryAllBalancesResponse, QuerySpendableBalancesRequest, QuerySpendableBalancesResponse, QueryTotalSupplyRequest, QueryTotalSupplyResponse, QuerySupplyOfRequest, QuerySupplyOfResponse, QueryParamsRequest, QueryParamsResponse, QueryDenomMetadataRequest, QueryDenomMetadataResponse, QueryDenomsMetadataRequest, QueryDenomsMetadataResponse, QueryDenomOwnersRequest, QueryDenomOwnersResponse } from "./query";
+ */
 
 /**
  *
@@ -13,10 +22,86 @@ import { GenericParseContext } from "../../encoding";
 export function createQueryHelperCreator(
     context: GenericParseContext,
     service: ProtoService,
-    methodKey: string,
-    helperCreatorName: string
+    methodKey?: string,
+    helperCreatorName?: string
 ) {
-    return t.returnStatement(t.identifier(helperCreatorName));
+    const typeAliases = Object.keys(service.methods).map((method) => {
+        return ast.tsTypeAliasDeclaration(
+            ast.identifier(`Use${method}Query`),
+            // No type parameters for type alias
+            ast.tsTypeParameterDeclaration([null]),
+            ast.tsTypeReference(
+                ast.identifier("UseQueryParams"),
+                ast.tsTypeParameterInstantiation([
+                    ast.tsTypeReference(
+                        ast.identifier(service.methods[method].requestType)
+                    ),
+                    ast.tsTypeReference(
+                        ast.identifier(service.methods[method].responseType)
+                    ),
+                ])
+            )
+        );
+    });
+
+    const createrFunctions = Object.keys(service.methods).map((method) => {
+        const callExpression = ast.callExpression(
+            ast.identifier("buildQuery"),
+            [
+                ast.objectExpression([
+                    ast.objectProperty(
+                        ast.identifier("reqEncoderFn"),
+                        ast.memberExpression(
+                            ast.identifier(`Query${method}Request`),
+                            ast.identifier("encode")
+                        )
+                    ),
+                    ast.objectProperty(
+                        ast.identifier("resDecoderFn"),
+                        ast.memberExpression(
+                            ast.identifier(`Query${method}Response`),
+                            ast.identifier("decode")
+                        )
+                    ),
+                    ast.objectProperty(
+                        ast.identifier("service"),
+                        ast.stringLiteral("cosmos.bank.v1beta1.Query")
+                    ),
+                    ast.objectProperty(
+                        ast.identifier("method"),
+                        ast.stringLiteral(method)
+                    ),
+                    ast.objectProperty(
+                        ast.identifier("getRpcInstance"),
+                        ast.identifier("getRpcInstance")
+                    ),
+                ]),
+            ]
+        );
+        callExpression.typeParameters = ast.tsTypeParameterInstantiation([
+            ast.tsTypeReference(ast.identifier(`Query${method}Request`)),
+            ast.tsTypeReference(ast.identifier(`Query${method}Response`)),
+        ]);
+
+        const customHookArgumentsType = ast.tsTypeAnnotation(
+            //TODO: Improvements, Figure out how to write ast code to generate the function expression below instead of hard coding the strong.
+            ast.tsTypeReference(ast.identifier("() => Rpc | undefined"))
+        );
+        const arg = ast.identifier("getRpcInstance");
+
+        arg.typeAnnotation = customHookArgumentsType;
+
+        const arrowFuncExp = ast.arrowFunctionExpression([arg], callExpression);
+
+        return ast.variableDeclaration("const", [
+            ast.variableDeclarator(
+                ast.identifier(`createGet${method}`),
+                arrowFuncExp
+            ),
+        ]);
+    });
+
+    return ast.program([...typeAliases, ...createrFunctions]);
 }
 
 /**
@@ -31,9 +116,39 @@ export function createQueryHelperCreator(
 export function createQueryHooks(
     context: GenericParseContext,
     service: ProtoService,
-    methodKey: string,
-    helperCreatorName: string,
-    hookName: string
+    methodKey?: string,
+    helperCreatorName?: string,
+    hookName?: string
 ) {
-    return t.returnStatement(t.identifier(hookName));
+    const hooks = Object.keys(service.methods).map((method) => {
+        const callExpression = ast.callExpression(
+            ast.identifier("buildUseQuery"),
+            [
+                ast.objectExpression([
+                    ast.objectProperty(
+                        ast.identifier("builderQueryFn"),
+                        ast.identifier(`createGet${method}`)
+                    ),
+                    ast.objectProperty(
+                        ast.identifier("queryKeyPrefix"),
+                        ast.stringLiteral(`${method}Query`)
+                    ),
+                ]),
+            ]
+        );
+
+        callExpression.typeParameters = ast.tsTypeParameterInstantiation([
+            ast.tsTypeReference(ast.identifier(`Query${method}Request`)),
+            ast.tsTypeReference(ast.identifier(`Query${method}Response`)),
+        ]);
+
+        return ast.variableDeclaration("const", [
+            ast.variableDeclarator(
+                ast.identifier(`use${method}`),
+                callExpression
+            ),
+        ]);
+    });
+
+    return ast.program([...hooks]);
 }
