dfdab6e53c64edd82c35f7ddb2d67989e1a6e44e
Gefei Hou <noroldburden@gmail.com>
2024-12-09 15:16:52 +0800

    Fix logics related to default config file for transpile

--- packages/telescope/src/commands/transpile.ts
+++ packages/telescope/src/commands/transpile.ts
@@ -12,163 +12,171 @@ export default async (argv: {
   let options: {
     [key: string]: unknown
   } = {}
-  let protoDirs
-  let outPath
-  let data: any
-  try{
-  //check if there's valid .telescope.json file to use
-  data = JSON.parse(readFileSync('./.telescope.json', {
-    encoding: 'utf8'
-  }))
-  } catch(e){}
+  let protoDirs;
+  let outPath;
+  let data: any;
+  let overrideConfig = true;
 
-  if (data?.protoDirs && data?.outPath && data?.options) {
-    protoDirs = data.protoDirs
-    outPath = data.outPath
-    options = data.options
+  if (argv.useDefaults) {
+    const defaultOptions = { ...defaultTelescopeOptions };
+
+    dotty.remove(defaultOptions, "aminoEncoding");
+    dotty.remove(defaultOptions, "packages");
+
+    options = defaultOptions;
   } else {
-    if (argv.useDefaults) {
-      const defaultOptions = { ...defaultTelescopeOptions };
+    options = {
+      // global options (can be overridden through plugins)
+      interfaces: {
+        enabled: false,
+        useByDefault: false,
+        useUnionTypes: false,
+      },
 
-      dotty.remove(defaultOptions, "aminoEncoding");
-      dotty.remove(defaultOptions, "packages");
-
-      options = defaultOptions;
-    } else {
-      options = {
-        // global options (can be overridden through plugins)
-        interfaces: {
-          enabled: false,
-          useByDefault: false,
-          useUnionTypes: false,
+      prototypes: {
+        enabled: true,
+        parser: {
+          keepCase: false
         },
-
-        prototypes: {
-          enabled: true,
-          parser: {
-            keepCase: false
-          },
-          methods: {
-            fromJSON: false,
-            toJSON: false,
-            encode: true,
-            decode: true,
-            fromPartial: true,
-            toAmino: true,
-            fromAmino: true,
-            fromProto: true,
-            toProto: true
-          },
-          addTypeUrlToObjects: true,
-          addTypeUrlToDecoders: true,
-
-          typingsFormat: {
-            duration: 'duration',
-            timestamp: 'date',
-            useExact: false,
-            useDeepPartial: false,
-            num64: 'bigint',
-            customTypes: {
-              useCosmosSDKDec: true
-            }
-          },
+        methods: {
+          fromJSON: false,
+          toJSON: false,
+          encode: true,
+          decode: true,
+          fromPartial: true,
+          toAmino: true,
+          fromAmino: true,
+          fromProto: true,
+          toProto: true
         },
+        addTypeUrlToObjects: true,
+        addTypeUrlToDecoders: true,
 
-        bundle: {
-          enabled: true
+        typingsFormat: {
+          duration: 'duration',
+          timestamp: 'date',
+          useExact: false,
+          useDeepPartial: false,
+          num64: 'bigint',
+          customTypes: {
+            useCosmosSDKDec: true
+          }
         },
+      },
 
-        stargateClients: {
-          enabled: true,
-          includeCosmosDefaultTypes: true
-        },
+      bundle: {
+        enabled: true
+      },
 
-        aminoEncoding: {
-          enabled: true,
-        },
+      stargateClients: {
+        enabled: true,
+        includeCosmosDefaultTypes: true
+      },
 
-        lcdClients: {
-          enabled: true
-        },
+      aminoEncoding: {
+        enabled: true,
+      },
 
-        rpcClients: {
-          enabled: true,
-          camelCase: true
-        }
+      lcdClients: {
+        enabled: true
+      },
+
+      rpcClients: {
+        enabled: true,
+        camelCase: true
       }
-
     }
 
-    const questions = [
-      {
-        _: true,
-        type: 'path',
-        name: 'protoDirs',
-        message: 'where is the proto directory?',
-        default: './proto'
-      },
-      {
-        _: true,
-        type: 'path',
-        name: 'outPath',
-        message: 'where is the output directory?',
-        default: './src/codegen'
+  }
+
+  const questions = [
+    {
+      _: true,
+      type: 'path',
+      name: 'protoDirs',
+      message: 'where is the proto directory?',
+      default: './proto'
+    },
+    {
+      _: true,
+      type: 'path',
+      name: 'outPath',
+      message: 'where is the output directory?',
+      default: './src/codegen'
+    }
+  ];
+
+  if (argv.config) {
+    overrideConfig = false;
+    const { config } = argv;
+    const configs = Array.isArray(config) ? config : [config];
+    const inputConfigFullPaths = configs.map(c => path.resolve(c));
+    let configJson;
+
+    for (const inputConfigPath of inputConfigFullPaths) {
+      try {
+        const configText = readFileSync(inputConfigPath, {
+          encoding: 'utf8'
+        })
+        if (configJson) {
+          configJson = deepmerge(configJson, JSON.parse(configText));
+        } else {
+          configJson = JSON.parse(configText);
+        }
+      } catch (ex) {
+        console.log(ex);
+        throw new Error("Must provide a .json file for --config.");
       }
+    }
+
+    // append protoDirs in config to argv.protoDirs
+    argv.protoDirs = [
+      ...(argv.protoDirs
+        ? Array.isArray(argv.protoDirs)
+          ? argv.protoDirs
+          : [argv.protoDirs]
+        : []),
+      ...(configJson.protoDirs ?? []),
     ];
 
-    if (argv.config) {
-      const { config } = argv;
-      const configs = Array.isArray(config) ? config : [config];
-      const inputConfigFullPaths = configs.map(c => path.resolve(c));
-      let configJson;
-
-      for (const inputConfigPath of inputConfigFullPaths) {
-        try {
-          const configText = readFileSync(inputConfigPath, {
-            encoding: 'utf8'
-          })
-          if (configJson) {
-            configJson = deepmerge(configJson, JSON.parse(configText));
-          } else {
-            configJson = JSON.parse(configText);
-          }
-        } catch (ex) {
-          console.log(ex);
-          throw new Error("Must provide a .json file for --config.");
-        }
-      }
-
-      // append protoDirs in config to argv.protoDirs
-      argv.protoDirs = [
-        ...(argv.protoDirs
-          ? Array.isArray(argv.protoDirs)
-            ? argv.protoDirs
-            : [argv.protoDirs]
-          : []),
-        ...(configJson.protoDirs ?? []),
-      ];
-
-      if (configJson.outPath) {
-        argv.outPath = configJson.outPath;
-      }
-
-      // For now, useDefaults will be override by --config
-      if (configJson.options) {
-        options = configJson.options;
-      }
+    if (configJson.outPath) {
+      argv.outPath = configJson.outPath;
     }
 
-    {
-      ({ protoDirs, outPath } = await prompt(questions, argv));
+    // For now, useDefaults will be override by --config
+    if (configJson.options) {
+      options = configJson.options;
     }
+  }
 
-    if (!Array.isArray(protoDirs)) {
-      protoDirs = [protoDirs];
+  if (!argv.config && !argv.useDefaults) {
+    try {
+      //check if there's valid .telescope.json file to use
+      data = JSON.parse(readFileSync('./.telescope.json', {
+        encoding: 'utf8'
+      }))
+    } catch (e) { }
+
+    //if there's existing valid .telescope.json, use and keep the file
+    if (data?.protoDirs && data?.outPath && data?.options) {
+      overrideConfig = false
+      protoDirs = data.protoDirs
+      outPath = data.outPath
+      options = data.options
     }
+  }
+  if (!protoDirs || !outPath) {
+    ({ protoDirs, outPath } = await prompt(questions, argv));
+  }
 
-    // remove any duplicate protodirs
-    protoDirs = [...new Set(protoDirs)];
+  if (!Array.isArray(protoDirs)) {
+    protoDirs = [protoDirs];
+  }
 
+  // remove any duplicate protodirs
+  protoDirs = [...new Set(protoDirs)];
+
+  if (overrideConfig) {
     writeFileSync(
       process.cwd() + '/.telescope.json',
       JSON.stringify(
